26a882d316ca48f97db76486b728121604229abc
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2019-11-28 14:28:56 +0100

    Updated references

---
+++ src/Navigator/NavigatorServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Navigator.Core;
+using Navigator.Core.Abstractions;
+
+namespace Navigator
+{
+    public static class NavigatorServiceCollectionExtensions
+    {
+        public static INavigatorBuilder AddNavigator(this IServiceCollection services)
+        {
+            var builder = new NavigatorBuilder(services);
+    
+            return builder;
+        }
+    }
+}
