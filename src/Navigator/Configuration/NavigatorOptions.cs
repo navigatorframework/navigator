af3f070345ae90c67d732530a90e85a86a8b6133
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-09 19:04:28 +0100

    Updated NavigatorOptions

--- src/Navigator/Configuration/NavigatorOptions.cs
+++ src/Navigator/Configuration/NavigatorOptions.cs
@@ -1,7 +1,10 @@
+using MihaZupan.TelegramBotClients.RateLimitedClient;
+
 namespace Navigator.Configuration
 {
     public class NavigatorOptions
     {
+        public SchedulerSettings SchedulerSettings { get; set; }
         public string BotToken { get; set; }
         public string BotWebHookUrl { get; set; }
     }
