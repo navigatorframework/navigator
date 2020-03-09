4a89f2b5e2e1fb6517db3d54169d8d9c7f61755e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-09 17:24:09 +0100

    Created NavigatorOptions

---
+++ src/Navigator/Configuration/NavigatorOptions.cs
@@ -0,0 +1,8 @@
+namespace Navigator.Configuration
+{
+    public class NavigatorOptions
+    {
+        public string BotToken { get; set; }
+        public string BotWebHookUrl { get; set; }
+    }
+}
