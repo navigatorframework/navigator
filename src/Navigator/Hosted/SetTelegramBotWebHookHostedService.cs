7231d1bb3634275c62520cd9ecf9ab2743a0ac87
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-09 19:06:52 +0100

    Finished implementation of hosted service

--- src/Navigator/Hosted/SetTelegramBotWebHookHostedService.cs
+++ src/Navigator/Hosted/SetTelegramBotWebHookHostedService.cs
@@ -1,14 +1,49 @@
+using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Navigator.Client;
+using Navigator.Configuration;
 
 namespace Navigator.Hosted
 {
     public class SetTelegramBotWebHookHostedService : BackgroundService
     {
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        private readonly ILogger<SetTelegramBotWebHookHostedService> _logger;
+        private readonly IServiceScopeFactory _serviceScopeFactory;
+        private readonly string _botWebHookUrl;
+        
+        public SetTelegramBotWebHookHostedService(ILogger<SetTelegramBotWebHookHostedService> logger, IServiceScopeFactory serviceScopeFactory,
+            IOptions<NavigatorOptions> navigatorOptions)
         {
-            throw new System.NotImplementedException();
+            _logger = logger;
+            _serviceScopeFactory = serviceScopeFactory;
+
+            if (string.IsNullOrWhiteSpace(navigatorOptions.Value.BotWebHookUrl))
+            {
+                throw new ArgumentNullException(nameof(navigatorOptions), "An URL for WebHook is required.");
+            }
+            
+            _botWebHookUrl = navigatorOptions.Value.BotWebHookUrl;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogTrace("starting with setup of webhook.");
+            _logger.LogTrace("Using url {WebHookUrl}", _botWebHookUrl);
+            
+            using var scope = _serviceScopeFactory.CreateScope();
+            
+            var botClient = scope.ServiceProvider.GetRequiredService<IBotClient>();
+            
+            await botClient.SetWebhookAsync(_botWebHookUrl, cancellationToken: stoppingToken);
+            
+            var me = await botClient.GetMeAsync(stoppingToken);
+
+            _logger.LogInformation($"Telegram Bot Client is receiving updates for bot: @{me.Username} at the url: {_botWebHookUrl}");
         }
     }
 }
