802548ecec9ed268270e03f345c7a07f8425c64d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-10 15:35:44 +0100

    Refactored BotClient

--- src/Navigator/Client/BotClient.cs
+++ src/Navigator/Client/BotClient.cs
@@ -1,22 +1,16 @@
 using System.Net;
 using System.Net.Http;
+using Microsoft.Extensions.Options;
 using MihaZupan.TelegramBotClients;
 using MihaZupan.TelegramBotClients.RateLimitedClient;
+using Navigator.Configuration;
 using Telegram.Bot;
 
 namespace Navigator.Client
 {
     public class BotClient : RateLimitedTelegramBotClient
     {
-        public BotClient(ITelegramBotClient botClient, SchedulerSettings schedulerSettings = null) : base(botClient, schedulerSettings)
-        {
-        }
-
-        public BotClient(string token, HttpClient httpClient = null, SchedulerSettings schedulerSettings = null) : base(token, httpClient, schedulerSettings)
-        {
-        }
-
-        public BotClient(string token, IWebProxy webProxy, SchedulerSettings schedulerSettings = null) : base(token, webProxy, schedulerSettings)
+        public BotClient(IOptions<NavigatorOptions> options) : base(options.Value.BotToken, (HttpClient) null, options.Value.SchedulerSettings)
         {
         }
     }
