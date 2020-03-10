7b43545d04c129c0f558354c2fa2f9abd9a2dc06
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-10 16:44:34 +0100

    Added IActionLauncher

---
+++ src/Navigator/Abstraction/IActionLauncher.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace Navigator.Abstraction
+{
+    public interface IActionLauncher
+    {
+        Task Launch(Update update);
+    }
+}
