9c90afa6723bbee6326c3bab53836253857836ff
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-10 10:37:20 +0100

    Implemented EndpointRouteBuilderExtensions

--- src/Navigator/EndpointRouteBuilderExtensions.cs
+++ src/Navigator/EndpointRouteBuilderExtensions.cs
@@ -1,7 +1,28 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Navigator.Configuration;
+
 namespace Navigator
 {
     public static class EndpointRouteBuilderExtensions
     {
-        
+        public static void MapNavigator(this IEndpointRouteBuilder endpointRouteBuilder)
+        {
+            using var scope = endpointRouteBuilder.ServiceProvider.CreateScope();
+            
+            var options = scope.ServiceProvider.GetRequiredService<IOptions<NavigatorOptions>>();
+
+            endpointRouteBuilder.MapPost(options.Value.EndpointWebHook, ProcessTelegramUpdate);
+        }
+
+        private static async Task ProcessTelegramUpdate(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
