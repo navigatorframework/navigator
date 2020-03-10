7d7bc59007e60db451cdebadaa14ce1c15c112cc
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-10 16:44:21 +0100

    Implemented base action

---
+++ src/Navigator/Actions/Action.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Navigator.Abstraction;
+
+namespace Navigator.Actions
+{
+    public abstract class Action : IAction
+    {
+        public DateTime Timestamp { get; }
+
+        protected Action()
+        {
+            Timestamp = DateTime.UtcNow;
+        }
+    }
+}
