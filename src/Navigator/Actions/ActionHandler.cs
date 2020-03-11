9d9fcc340ab224241d5bafd6946ba462621d45cd
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-11 16:08:19 +0100

    Implemented ActionHandler

--- src/Navigator/Actions/ActionHandler.cs
+++ src/Navigator/Actions/ActionHandler.cs
@@ -1,7 +1,19 @@
-﻿namespace Navigator.Actions
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Navigator.Abstraction;
+
+namespace Navigator.Actions
 {
-    public class ActionHandler
+    public abstract class ActionHandler<TAction> : IRequestHandler<TAction> where TAction : IAction
     {
-        
+        public readonly INavigatorContext Ctx;
+
+        public ActionHandler(INavigatorContext ctx)
+        {
+            Ctx = ctx;
+        }
+
+        public abstract Task<Unit> Handle(TAction request, CancellationToken cancellationToken);
     }
 }
