1de0f3b80339dd38ef56567ca1a54e9a84ddeeba
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-11 13:44:33 +0100

    Refactored NavigatorMiddleware

--- src/Navigator/NavigatorMiddleware.cs
+++ src/Navigator/NavigatorMiddleware.cs
@@ -7,32 +7,38 @@ using Microsoft.Extensions.Logging;
 using Navigator.Abstraction;
 using Telegram.Bot.Types;
 
-namespace Navigator.Middleware
+namespace Navigator
 {
     public class NavigatorMiddleware : INavigatorMiddleware
     {
         protected readonly ILogger<NavigatorMiddleware> Logger;
         protected readonly INotificationLauncher NotificationLauncher;
-        protected readonly IMediator Mediator;
+        protected readonly IActionLauncher ActionLauncher;
+        protected readonly INavigatorContext NavigatorContext;
 
-        public NavigatorMiddleware(ILogger<NavigatorMiddleware> logger, INotificationLauncher notificationLauncher)
+        public NavigatorMiddleware(ILogger<NavigatorMiddleware> logger, INotificationLauncher notificationLauncher, IActionLauncher actionLauncher, INavigatorContext navigatorContext)
         {
             Logger = logger;
             NotificationLauncher = notificationLauncher;
+            ActionLauncher = actionLauncher;
+            NavigatorContext = navigatorContext;
         }
 
         public async Task Handle(HttpRequest httpRequest)
         {
             var update = await TryGetTelegramUpdate(httpRequest.Body);
-
             if (update == null) return;
 
-            await NotificationLauncher.Launch(update);
+            await NavigatorContext.Init(update);
+            
+            await NotificationLauncher.Launch();
+            await ActionLauncher.Launch();
         }
         
         protected async Task<Update?> TryGetTelegramUpdate(Stream stream)
         {
             Update update = null;
+            
             try
             {
                 update = await JsonSerializer.DeserializeAsync<Update>(stream);
