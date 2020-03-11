be916eccba8f3b37beb424ec6600345724782bdf
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-11 16:09:08 +0100

    Refactored NavigatorServiceCollectionExtensions Now we add the actions too

--- src/Navigator/NavigatorServiceCollectionExtensions.cs
+++ src/Navigator/NavigatorServiceCollectionExtensions.cs
@@ -1,25 +1,38 @@
 using System;
+using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using Navigator.Abstraction;
 using Navigator.Configuration;
 using Navigator.Hosted;
+using Scrutor;
 
 namespace Navigator
 {
     public static class NavigatorServiceCollectionExtensions
     {
-        public static IServiceCollection AddNavigator(this IServiceCollection services, Action<NavigatorOptions> navigatorOptions)
+        public static IServiceCollection AddNavigator(this IServiceCollection services, Action<NavigatorOptions> options, params Assembly[] actionAssemblies)
         {
-            if (navigatorOptions == null)
+            if (options == null)
             {
-                throw new ArgumentNullException(nameof(navigatorOptions), "Navigator options are required for navigator framework to work.");
+                throw new ArgumentNullException(nameof(options), "Navigator options are required for navigator framework to work.");
             }
-            services.Configure(navigatorOptions);
+            
+            services.Configure(options);
+            
+            services.AddSingleton<IBotClient, BotClient>();
 
             services.AddHostedService<SetTelegramBotWebHookHostedService>();
-            services.AddScoped<IBotClient, BotClient>();
             services.AddScoped<INotificationLauncher, NotificationLauncher>();
+            services.AddScoped<IActionLauncher, ActionLauncher>();
+            services.AddScoped<INavigatorContext, NavigatorContext>();
             services.AddScoped<INavigatorMiddleware, NavigatorMiddleware>();
+            
+            services.Scan(scan => scan
+                .FromAssemblies(actionAssemblies)
+                .AddClasses(classes => classes.AssignableTo<IAction>())
+                .UsingRegistrationStrategy(RegistrationStrategy.Append)
+                .AsImplementedInterfaces()
+                .WithScopedLifetime());
             
             return services;
         }
