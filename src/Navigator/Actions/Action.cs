6388d9621d6fade4bb3893e10f2728e31fb8a8c0
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-11 11:37:18 +0100

    Updated Action, added type and order

--- src/Navigator/Actions/Action.cs
+++ src/Navigator/Actions/Action.cs
@@ -5,6 +5,8 @@ namespace Navigator.Actions
 {
     public abstract class Action : IAction
     {
+        public int Order { get; } = 1000;
+        public string Type { get; }
         public DateTime Timestamp { get; }
 
         protected Action()
