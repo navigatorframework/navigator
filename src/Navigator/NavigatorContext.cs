ed466f02e2e9fa3bae0d2f8db8a69d7054e8fb69
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-11 11:36:51 +0100

    Basic NavigatorContext

---
+++ src/Navigator/NavigatorContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Navigator.Abstraction;
+using Telegram.Bot.Types;
+
+namespace Navigator
+{
+    public class NavigatorContext
+    {
+        protected readonly Dictionary<Type, object> Extensions;
+        public readonly IBotClient Client;
+        public readonly User BotProfile;
+        public readonly Update Update;
+
+        public NavigatorContext(Dictionary<Type, object> extensions, IBotClient client, User botProfile, Update update)
+        {
+            Extensions = extensions;
+            Client = client;
+            BotProfile = botProfile;
+            Update = update;
+        }
+        
+        public TExtension Get<TExtension>(bool throwIfNotFound)
+        {
+            if (Extensions.TryGetValue(typeof(TExtension), out var extension))
+            {
+                if (extension is TExtension castedExtension)
+                {
+                    return castedExtension;
+                }
+            }
+
+            return throwIfNotFound 
+                ? throw new KeyNotFoundException($"{typeof(TExtension).Name} was not found.") 
+                : (TExtension) default;
+        }
+    }
+}
