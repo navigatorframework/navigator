75c0e9d9fd201c720d4970b0cfecf147c4d5898a
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-11 11:47:37 +0100

    Added INavigatorContextFactory

---
+++ src/Navigator/Abstraction/INavigatorContextFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace Navigator.Abstraction
+{
+    public interface INavigatorContextFactory
+    {
+        Task<NavigatorContext> GetContext(Update update);
+    }
+}
