63bdd9ded20a7574ae1edd88f4736f9af4204058
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-12 19:00:11 +0100

    Implemented NavigatorContextBuilder

---
+++ src/Navigator/NavigatorContextBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Navigator.Abstraction;
+using Telegram.Bot.Types;
+
+namespace Navigator
+{
+    public class NavigatorContextBuilder : INavigatorContextBuilder
+    {
+        protected readonly ILogger<NavigatorContextBuilder> Logger;
+        protected readonly IEnumerable<INavigatorContextExtensionProvider> ExtensionProviders;
+        protected readonly INavigatorContext Ctx;
+        public NavigatorContextBuilder(ILogger<NavigatorContextBuilder> logger, IEnumerable<INavigatorContextExtensionProvider> extensionProviders, INavigatorContext ctx)
+        {
+            Logger = logger;
+            ExtensionProviders = extensionProviders;
+            Ctx = ctx;
+        }
+
+        public async Task Build(Update update)
+        {
+            var extensions = new Dictionary<Type, object>();
+
+            foreach (var extensionProvider in ExtensionProviders)
+            {
+                var (type, extension) = await extensionProvider.Process(update);
+
+                if (type != null && extension != null)
+                {
+                    extensions.Add(type, extension);
+                }
+            }
+
+            await Ctx.Init(update, extensions);
+        }
+    }
+}
