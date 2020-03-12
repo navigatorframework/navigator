5f3784db59ba24d4f87487ce0f8e84b8c5c16f0e
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-12 18:59:06 +0100

    Added INavigatorContextExtensionProvider

---
+++ src/Navigator/Abstraction/INavigatorContextExtensionProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+
+namespace Navigator.Abstraction
+{
+    public interface INavigatorContextExtensionProvider
+    {
+        Task<(Type?, object?)> Process(Update update);
+    }
+}
