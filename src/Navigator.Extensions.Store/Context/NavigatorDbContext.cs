bff25db870486af2eda37db5f3d4d968a1167f2f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-15 22:37:02 +0100

    Linted NavigatorDbContext

--- src/Navigator.Extensions.Store/Context/NavigatorDbContext.cs
+++ src/Navigator.Extensions.Store/Context/NavigatorDbContext.cs
@@ -1,4 +1,4 @@
-﻿﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Navigator.Extensions.Store.Context.Configuration;
 using Navigator.Extensions.Store.Entity;
 
@@ -6,11 +6,11 @@ namespace Navigator.Extensions.Store.Context
 {
     public class NavigatorDbContext : NavigatorDbContext<User, Chat>
     {
-        
     }
 
     public class NavigatorDbContext<TUser, TChat> : DbContext
-     where TUser : User where TChat : Chat
+        where TUser : User
+        where TChat : Chat
     {
         public DbSet<TUser> Users;
         public DbSet<TChat> Chats;
@@ -22,7 +22,6 @@ namespace Navigator.Extensions.Store.Context
 
         public NavigatorDbContext(DbContextOptions options) : base(options)
         {
-            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -30,7 +29,7 @@ namespace Navigator.Extensions.Store.Context
             modelBuilder.ApplyConfiguration(new ConversationEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new ChatEntityTypeConfiguration<TChat>());
             modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration<TUser>());
-            
+
             base.OnModelCreating(modelBuilder);
         }
     }
