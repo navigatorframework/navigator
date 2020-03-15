2c65740eaa0f5116ce2ab305cdb59b89560968c9
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-15 20:50:46 +0100

    Updated service collection extensions

--- src/Navigator.Extensions.Store/NavigatorServiceCollectionExtensions.cs
+++ src/Navigator.Extensions.Store/NavigatorServiceCollectionExtensions.cs
@@ -1,24 +1,65 @@
 ﻿﻿using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Navigator.Extensions.Store.Context;
+ using Microsoft.Extensions.DependencyInjection.Extensions;
+ using Microsoft.Extensions.Options;
+ using Navigator.Abstraction;
+ using Navigator.Extensions.Store.Abstraction;
+ using Navigator.Extensions.Store.Configuration;
+ using Navigator.Extensions.Store.Context;
 using Navigator.Extensions.Store.Entity;
+ using Navigator.Extensions.Store.Provider;
 
-namespace Navigator.Extensions.Store
+ namespace Navigator.Extensions.Store
 {
     public static class NavigatorServiceCollectionExtensions
     {
-        public static IServiceCollection AddNavigatorStore(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction = null)
+        public static IServiceCollection AddNavigatorStore(this IServiceCollection services, Action<DbContextOptionsBuilder> dbContextOptions = null,
+            Action<NavigatorStoreOptions> options = null)
         {
-            return services.AddNavigatorStore<NavigatorDbContext, User, Chat>(optionsAction);
+            return services.AddNavigatorStore<NavigatorDbContext, User, Chat>(dbContextOptions, options);
         }
-        public static IServiceCollection AddNavigatorStore<TContext, TUser, TChat>(this IServiceCollection services, Action<DbContextOptionsBuilder> optionsAction = null)
-            where TContext : NavigatorDbContext<TUser, TChat>
+        
+        public static IServiceCollection AddNavigatorStore<TContext, TUser>(this IServiceCollection services, Action<DbContextOptionsBuilder> dbContextOptions = null,
+            Action<NavigatorStoreOptions> options = null)
+            where TContext : NavigatorDbContext<TUser, Chat> 
+            where TUser : User
+        {
+            return services.AddNavigatorStore<TContext, TUser, Chat>(dbContextOptions, options);
+        }
+        
+        public static IServiceCollection AddNavigatorStore<TContext, TUser, TChat>(this IServiceCollection services, Action<DbContextOptionsBuilder> dbContextOptions = null,
+            Action<NavigatorStoreOptions> options = null) 
+            where TContext : NavigatorDbContext<TUser, TChat> 
             where TUser : User
             where TChat : Chat
         {
-            services.AddDbContext<TContext>(optionsAction);
+            services.Configure(options);
+            services.AddDbContext<TContext>(dbContextOptions);
 
+            var storeOptions = new NavigatorStoreOptions();
+            options(storeOptions);
+            
+            if (typeof(IChatMapper<TChat>).IsAssignableFrom(storeOptions.ChatMapper))
+            {
+                services.AddScoped(typeof(IChatMapper<TChat>), storeOptions.ChatMapper);
+            }
+            else
+            {
+                throw new ArgumentException("TODO");
+            }
+
+            if (typeof(IUserMapper<TUser>).IsAssignableFrom(storeOptions.UserMapper))
+            {
+                services.AddScoped(typeof(IUserMapper<TUser>), storeOptions.UserMapper);
+            }
+            else
+            {
+                throw new ArgumentException("TODO");
+            }
+            
+            services.TryAddEnumerable(ServiceDescriptor.Scoped<INavigatorContextExtensionProvider, DefaultUserContextProvider<TContext, TUser, TChat>>());
+            
             return services;
         }
     }
