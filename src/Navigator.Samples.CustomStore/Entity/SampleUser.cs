a513bd9e00d8b14a2d24f3a68185f55907ca9624
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-15 22:37:17 +0100

    Added mapper to SampleUser

--- src/Navigator.Samples.CustomStore/Entity/SampleUser.cs
+++ src/Navigator.Samples.CustomStore/Entity/SampleUser.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Navigator.Extensions.Store.Abstraction;
 using Navigator.Extensions.Store.Entity;
 
 namespace Navigator.Samples.CustomStore.Entity
@@ -15,6 +16,21 @@ namespace Navigator.Samples.CustomStore.Entity
         {
             builder.Property(e => e.Secret)
                 .HasMaxLength(100);
+        }
+    }
+    
+    public class SampleUserMapper : IUserMapper<SampleUser>
+    {
+        public SampleUser Parse(Telegram.Bot.Types.User user)
+        {
+            return new SampleUser
+            {
+                Id = user.Id,
+                IsBot = user.IsBot,
+                LanguageCode = user.LanguageCode,
+                Username = user.Username,
+                Secret = "no secret"
+            };
         }
     }
 }
