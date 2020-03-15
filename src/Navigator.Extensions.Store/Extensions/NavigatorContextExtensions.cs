cfabf0986465b2fb48ead4aca547448e77de2422
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-15 22:41:37 +0100

    Added some extension methods for store extension

--- src/Navigator.Extensions.Store/Extensions/NavigatorContextExtensions.cs
+++ src/Navigator.Extensions.Store/Extensions/NavigatorContextExtensions.cs
@@ -1,16 +1,39 @@
-using Navigator.Extensions.Store.Provider;
+using System;
+using Navigator.Extensions.Store.Entity;
 
 namespace Navigator.Extensions.Store.Extensions
 {
     public static class NavigatorContextExtensions
     {
         public static string DefaultUserKey => "navigator.extensions.store.user";
+        public static string DefaultChatKey => "navigator.extensions.store.chat";
 
-        
-        
-        public static TUser GetUserOrDefault<TUser>(this NavigatorContext ctx, TUser tUserDefault = default)
+        #region User
+
+        public static TUser GetUser<TUser>(this NavigatorContext ctx) where TUser : User
         {
-            return ctx.Get<TUser>(DefaultUserKey) ?? tUserDefault;
+            return ctx.GetUserOrDefault<TUser>() ?? throw new Exception("User not found");
         }
+        
+        public static TUser? GetUserOrDefault<TUser>(this NavigatorContext ctx, TUser defaultValue = default) where TUser : User
+        {
+            return ctx.Get<TUser>(DefaultUserKey) ?? defaultValue;
+        }
+
+        #endregion
+
+        #region Chat
+
+        public static TChat GetChat<TChat>(this NavigatorContext ctx) where TChat : Chat
+        {
+            return ctx.GetChatOrDefault<TChat>() ?? throw new Exception("User not found");
+        }
+        
+        public static TChat? GetChatOrDefault<TChat>(this NavigatorContext ctx, TChat defaultValue = default) where TChat : Chat
+        {
+            return ctx.Get<TChat>(DefaultChatKey) ?? defaultValue;
+        }
+
+        #endregion
     }
 }
