aad7bacb802fb5657b72885e081f08930bfb293f
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2020-03-18 17:23:36 +0100

    Corrected CallbackQuery

--- src/Navigator.Extensions.Actions/CallbackQueryAction.cs
+++ src/Navigator.Extensions.Actions/CallbackQueryAction.cs
@@ -3,17 +3,20 @@ using Navigator.Actions;
 
 namespace Navigator.Extensions.Actions
 {
-    public class CallbackQueryAction : Action
+    public abstract class CallbackQueryAction : Action
     {
         public override string Type => ActionType.CallbackQuery;
+
+        public string Data { get; set; } = string.Empty;
+
+        public bool IsGameQuery { get; set; }
+
         public override IAction Init(INavigatorContext ctx)
         {
-            throw new System.NotImplementedException();
-        }
-
-        public override bool CanHandle(INavigatorContext ctx)
-        {
-            throw new System.NotImplementedException();
+            Data = ctx.Update.CallbackQuery.Data;
+            IsGameQuery = ctx.Update.CallbackQuery.IsGameQuery;
+            
+            return this;
         }
     }
 }
