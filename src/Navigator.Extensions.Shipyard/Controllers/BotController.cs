d6beb3b9956338ce9afb4a22548c102ce376b649
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-01-11 18:10:48 +0100

    Updated BotController

--- src/Navigator.Extensions.Shipyard/Controllers/BotController.cs
+++ src/Navigator.Extensions.Shipyard/Controllers/BotController.cs
@@ -1,47 +1,66 @@
-using System.IO;
-using System.Linq;
-using System.Net.Mime;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Navigator.Abstractions;
+using Navigator.Extensions.Shipyard.Abstractions;
+using Navigator.Extensions.Shipyard.Middleware;
 
 namespace Navigator.Extensions.Shipyard.Controllers
 {
+    /// <summary>
+    /// Bot controller.
+    /// </summary>
+    [Authorize(AuthenticationSchemes = nameof(ShipyardApiKeyAuthenticationHandler))]
     [ApiController]
     [ApiVersion("1")]
     [Route("shipyard/api/v{v:apiVersion}/[controller]")]
     public class BotController : ControllerBase
     {
-        [HttpGet("information")]
-        public async Task<IActionResult> GetBotInformation([FromServices] IBotClient _botClient)
+        private readonly IBotManagementService _botManagementService;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="botManagementService"></param>
+        public BotController(IBotManagementService botManagementService)
         {
-            var information = await _botClient.GetMeAsync();
+            _botManagementService = botManagementService;
+        }
+
+        [HttpGet("information")]
+        public async Task<IActionResult> GetBotInformation(CancellationToken cancellationToken)
+        {
+            var information = await _botManagementService.GetBotInfo(cancellationToken);
             
             return Ok(information);
         }
 
-        [HttpGet("botpic")]
-        public async Task<IActionResult> GetBotPic([FromServices] IBotClient botClient)
+        [HttpGet("pic")]
+        public async Task<IActionResult> GetBotPic(CancellationToken cancellationToken)
         {
-            
-            var photos = await botClient.GetUserProfilePhotosAsync(botClient.BotId);
+            var botPic = await _botManagementService.GetBotPic(cancellationToken);
 
-            if (photos.TotalCount > 0)
+            if (botPic is not null)
             {
-                var botpicId = photos.Photos.FirstOrDefault()?
-                    .OrderByDescending(x => x.FileSize).FirstOrDefault()
-                    ?.FileId;
-
-                if (!string.IsNullOrWhiteSpace(botpicId))
-                {
-                    var botpic = new MemoryStream();
-                    await botClient.GetInfoAndDownloadFileAsync(botpicId, botpic);
-                
-                    return File(botpic.ToArray(), "image/png");   
-                }
+                return File(botPic.File, botPic.MimeType);
             }
-
             return NotFound("BotPic not found");
         }
+
+        [HttpGet("commands")]
+        public async Task<IActionResult> GetBotCommands(CancellationToken cancellationToken)
+        {
+            var botCommands = await _botManagementService.GetBotCommands(cancellationToken);
+
+            return Ok(botCommands);
+        }
+
+        [HttpPut("commands")]
+        public async Task<IActionResult> UpdateBotCommands()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
