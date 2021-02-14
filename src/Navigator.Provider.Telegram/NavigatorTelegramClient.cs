8f0ed78f3c91788ac100dd501caaf87272b991dd
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-14 19:28:34 +0100

    Implemented GetBotUser on Telegram provider

--- src/Navigator.Provider.Telegram/NavigatorTelegramClient.cs
+++ src/Navigator.Provider.Telegram/NavigatorTelegramClient.cs
@@ -1,6 +1,9 @@
 using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 using MihaZupan.TelegramBotClients;
 using Navigator.Configuration;
+using Navigator.Entities;
 
 namespace Navigator.Provider.Telegram
 {
@@ -9,6 +12,17 @@ namespace Navigator.Provider.Telegram
         public NavigatorTelegramClient(NavigatorOptions options) 
             : base(options.GetTelegramToken(), (HttpClient) default!, options.GetSchedulerSettingsOrDefault())
         {
+        }
+
+        public async Task<BotUser> GetBotUser(CancellationToken cancellationToken = default)
+        {
+            var bot = await GetMeAsync(cancellationToken);
+
+            return new BotUser
+            {
+                Id = bot.Id.ToString(),
+                Username = bot.Username
+            };
         }
     }
 }
