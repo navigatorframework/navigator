dba4a6b26bc8511ed0a881fea5c384b8ebbdc5ca
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-14 13:05:42 +0100

    Updated INavigatorOptions

--- src/Navigator/INavigatorOptions.cs
+++ src/Navigator/INavigatorOptions.cs
@@ -1,4 +1,6 @@
-namespace Navigator.Abstractions
+using System.Collections.Generic;
+
+namespace Navigator
 {
     /// <summary>
     /// 
@@ -8,5 +10,7 @@ namespace Navigator.Abstractions
         bool TryRegisterOption(string key, object option);
         bool ForceRegisterOption(string key, object option);
         TType? RetrieveOption<TType>(string key);
+        Dictionary<string, object> RetrieveAllOptions();
+        void Import(Dictionary<string, object> options, bool overwrite = false);
     }
 }
