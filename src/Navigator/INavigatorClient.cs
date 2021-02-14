a99aa832a85e861683d0d0f2c594de1bcb5d1b0c
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-14 19:28:02 +0100

    Added new method to INavigatorClient

--- src/Navigator/INavigatorClient.cs
+++ src/Navigator/INavigatorClient.cs
@@ -1,7 +1,16 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Navigator.Entities;
+
 namespace Navigator
 {
     public interface INavigatorClient
     {
-        
+        /// <summary>
+        /// Retrieves the bot user information.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<BotUser> GetBotUser(CancellationToken cancellationToken = default);
     }
 }
