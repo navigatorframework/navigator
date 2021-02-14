452f6f60322ed867fc83024e9790c6ec7c5836e6
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-14 13:47:13 +0100

    Created NavigatorProviderConfiguration

---
+++ src/Navigator/Provider/NavigatorProviderConfiguration.cs
@@ -0,0 +1,37 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Navigator.Provider
+{
+    /// <summary>
+    /// Provides a entry point for configuring new providers for Navigator.
+    /// </summary>
+    public class NavigatorProviderConfiguration
+    {
+        private readonly NavigatorConfiguration _navigatorConfiguration;
+
+        /// <summary>
+        /// Default constructor for <see cref="NavigatorProviderConfiguration"/>.
+        /// </summary>
+        /// <param name="navigatorConfiguration"></param>
+        public NavigatorProviderConfiguration(NavigatorConfiguration navigatorConfiguration)
+        {
+            _navigatorConfiguration = navigatorConfiguration;
+        }
+
+        /// <summary>
+        /// Configure a new provider using this method.
+        /// </summary>
+        /// <param name="optionsAction"></param>
+        /// <param name="servicesAction"></param>
+        /// <returns></returns>
+        public NavigatorConfiguration Provider(Action<NavigatorOptions>? optionsAction, Action<IServiceCollection>? servicesAction)
+        {
+            optionsAction?.Invoke(_navigatorConfiguration.Options);
+            
+            servicesAction?.Invoke(_navigatorConfiguration.Services);
+
+            return _navigatorConfiguration;
+        }
+    }
+}
