be363e1422dce1766df6b6473e3ac557aa370d3d
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-22 23:29:58 +0100

    Corrected whitespace

--- src/Navigator.Providers.Telegram/TelegramProvider.cs
+++ src/Navigator.Providers.Telegram/TelegramProvider.cs
@@ -11,7 +11,7 @@ namespace Navigator.Providers.Telegram
         {
             _client = client;
         }
-
+        
         public INavigatorClient GetClient()
         {
             return _client;
