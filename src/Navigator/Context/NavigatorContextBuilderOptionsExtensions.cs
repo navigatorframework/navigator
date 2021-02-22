976f890b190164a040a082249849557be6237e7c
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-22 23:29:04 +0100

    Updated requirements for SetProvider extension

--- src/Navigator/Context/NavigatorContextBuilderOptionsExtensions.cs
+++ src/Navigator/Context/NavigatorContextBuilderOptionsExtensions.cs
@@ -8,7 +8,7 @@ namespace Navigator.Context
 
         private const string ProviderKey = "_navigator.context.options.provider";
 
-        public static void SetProvider<TProvider>(this INavigatorContextBuilderOptions contextBuilderOptions) where TProvider : IProvider, new()
+        public static void SetProvider<TProvider>(this INavigatorContextBuilderOptions contextBuilderOptions) where TProvider : IProvider
         {
             contextBuilderOptions.TryRegisterOption(ProviderKey, typeof(TProvider));
 
