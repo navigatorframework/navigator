40085849d51f08c554fb41292d8392fd3c7ef1af
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-23 12:35:15 +0100

    Updated MessageAction in Telegram Provider

--- src/Navigator.Providers.Telegram/Actions/MessageAction.cs
+++ src/Navigator.Providers.Telegram/Actions/MessageAction.cs
@@ -1,6 +1,8 @@
 using System.Threading.Tasks;
 using Navigator.Actions;
 using Navigator.Context;
+using Navigator.Context.Extensions;
+using Telegram.Bot.Types;
 
 namespace Navigator.Providers.Telegram.Actions
 {
@@ -8,5 +10,19 @@ namespace Navigator.Providers.Telegram.Actions
     {
         public override string Type { get; protected set; } = ActionHelper.Type.For<TelegramProvider>(nameof(MessageAction));
         public override ushort Priority { get; protected set; } = ActionHelper.Priority.Default;
+
+        public override IAction Init(INavigatorContext navigatorContext)
+        {
+            var update = navigatorContext.GetOriginalUpdateOrDefault<Update>();
+
+            if (update is not null)
+            {
+                Message = update.Message;
+            }
+
+            return this;    
+        }
+        
+        public Message Message { get; protected set; }
     }
 }
