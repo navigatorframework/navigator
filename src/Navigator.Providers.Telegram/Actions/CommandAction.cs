2f6fe39534e0490cf20b8ebdb05c34257a3a2f8c
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-02-23 12:36:52 +0100

    Implemented CommandAction

--- src/Navigator.Providers.Telegram/Actions/CommandAction.cs
+++ src/Navigator.Providers.Telegram/Actions/CommandAction.cs
@@ -1,11 +1,31 @@
 using Navigator.Actions;
 using Navigator.Context;
+using Navigator.Providers.Telegram.Extensions;
 
 namespace Navigator.Providers.Telegram.Actions
 {
-    public abstract class CommandAction : BaseAction
+    public abstract class CommandAction : MessageAction
     {
+        protected CommandAction()
+        {
+            Command = string.Empty;
+            Arguments = string.Empty;
+        }
+
         public override string Type { get; protected set; } = ActionHelper.Type.For<TelegramProvider>(nameof(CommandAction));
         public override ushort Priority { get; protected set; } = ActionHelper.Priority.Default;
+        
+        public override IAction Init(INavigatorContext navigatorContext)
+        {
+            Command = Message.ExtractCommand(navigatorContext.BotProfile.Username) ?? string.Empty;
+
+            Arguments = Message.ExtractArguments(navigatorContext.BotProfile.Username) ?? string.Empty;
+
+            return this;    
+        }
+        
+        public string Command { get; protected set; }
+        public string Arguments { get; protected set; }
+        
     }
 }
