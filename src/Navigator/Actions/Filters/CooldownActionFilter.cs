1021737ec3d0d046cfb8b881fed09700c0437914
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-03 22:17:19 +0100

    Started with CooldownActionFilter implementation

---
+++ src/Navigator/Actions/Filters/CooldownActionFilter.cs
@@ -0,0 +1,46 @@
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.Extensions.Caching.Distributed;
+using Navigator.Actions.Model;
+using Navigator.Context;
+
+namespace Navigator.Actions.Filters
+{
+    public class CooldownActionFilter<TAction> : ActionFilter<TAction> where TAction : IAction
+    {
+        private readonly IDistributedCache _cache;
+        private readonly INavigatorContext _navigatorContext;
+        public CooldownActionFilter(IDistributedCache cache, INavigatorContextAccessor navigatorContextAccessor)
+        {
+            _cache = cache;
+            _navigatorContext = navigatorContextAccessor.NavigatorContext;
+        }
+
+        public override async Task<ActionStatus> Handle(TAction action, CancellationToken cancellationToken, RequestHandlerDelegate<ActionStatus> next)
+        {
+            var key = GenerateKey(action);
+            
+            var cooldownStatus = await _cache.GetAsync<string>(key, cancellationToken);
+
+            if (cooldownStatus is not null)
+            {
+                return new ActionStatus(true);
+            }
+
+            var status = await next();
+
+            if (status.IsSuccess)
+            {
+                await _cache.SetStringAsync(key, key); //TODO add timer with attribute;
+            }
+
+            return status;
+        }
+
+        private string GenerateKey(TAction action)
+        {
+            return $"{action.Type}:{_navigatorContext.Conversation.Chat.Id}";
+        }
+    }
+}
