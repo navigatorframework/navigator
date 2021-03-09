66368ad6550f79e4f716ea4912d45a0efecfb058
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-09 21:52:19 +0100

    Corrected CommandAction

--- src/Navigator.Providers.Telegram/Actions/CommandAction.cs
+++ src/Navigator.Providers.Telegram/Actions/CommandAction.cs
@@ -14,11 +14,10 @@ namespace Navigator.Providers.Telegram.Actions
         public override string Type { get; protected set; } = ActionsHelper.Type.For<TelegramNavigatorProvider>(nameof(CommandAction));
 
         /// <inheritdoc />
-        public override ushort Priority { get; protected set; } = ActionsHelper.Priority.Default;
-
-        /// <inheritdoc />
         public override IAction Init(INavigatorContext navigatorContext)
         {
+            base.Init(navigatorContext);
+            
             Command = Message.ExtractCommand(navigatorContext.BotProfile.Username) ?? string.Empty;
 
             Arguments = Message.ExtractArguments(navigatorContext.BotProfile.Username);
