ff2641dd539dda06b2d6e13b415998a58e503c2c
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-09 18:10:39 +0100

    Improved CommandAction

--- src/Navigator.Providers.Telegram/Actions/CommandAction.cs
+++ src/Navigator.Providers.Telegram/Actions/CommandAction.cs
@@ -5,28 +5,36 @@ using Navigator.Providers.Telegram.Extensions;
 
 namespace Navigator.Providers.Telegram.Actions
 {
+    /// <summary>
+    /// Command based action.
+    /// </summary>
     public abstract class CommandAction : MessageAction
     {
-        protected CommandAction()
-        {
-            Command = string.Empty;
-            Arguments = string.Empty;
-        }
-
+        /// <inheritdoc />
         public override string Type { get; protected set; } = ActionsHelper.Type.For<TelegramNavigatorProvider>(nameof(CommandAction));
+
+        /// <inheritdoc />
         public override ushort Priority { get; protected set; } = ActionsHelper.Priority.Default;
-        
+
+        /// <inheritdoc />
         public override IAction Init(INavigatorContext navigatorContext)
         {
             Command = Message.ExtractCommand(navigatorContext.BotProfile.Username) ?? string.Empty;
 
-            Arguments = Message.ExtractArguments(navigatorContext.BotProfile.Username) ?? string.Empty;
+            Arguments = Message.ExtractArguments(navigatorContext.BotProfile.Username);
 
             return this;    
         }
         
-        public string Command { get; protected set; }
-        public string Arguments { get; protected set; }
+        /// <summary>
+        /// Command.
+        /// </summary>
+        public string Command { get; protected set; } = null!;
+        
+        /// <summary>
+        /// Any arguments passed with the command. If no arguments were passed, it will be null.
+        /// </summary>
+        public string? Arguments { get; protected set; }
         
     }
 }
