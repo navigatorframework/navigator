a95a345777591bf1992e2f4e803145fc3b6c3f85
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-09 14:41:32 +0100

    Leaving Discord in pause for now

--- src/Navigator.Providers.Discord/DiscordNavigatorProvider.cs
+++ src/Navigator.Providers.Discord/DiscordNavigatorProvider.cs
@@ -1,8 +1,15 @@
+using System.Threading.Tasks;
+
 namespace Navigator.Providers.Discord
 {
     public class DiscordNavigatorProvider : INavigatorProvider
     {
         public INavigatorClient GetClient()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task HandleReply()
         {
             throw new System.NotImplementedException();
         }
