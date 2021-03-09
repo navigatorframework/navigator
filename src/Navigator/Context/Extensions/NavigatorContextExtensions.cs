6de2257d9ab5c2dc09d04d750430c7dc417cf9af
Lucas Maximiliano Marino <lucasmmarino@icloud.com>
2021-03-09 16:49:43 +0100

    Added GetOriginalUpdate extension to NavigatorContextExtensions

--- src/Navigator/Context/Extensions/NavigatorContextExtensions.cs
+++ src/Navigator/Context/Extensions/NavigatorContextExtensions.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections.Generic;
 
 namespace Navigator.Context.Extensions
 {
     public static class NavigatorContextExtensions
     {
+        public static TUpdate GetOriginalUpdate<TUpdate>(this INavigatorContext navigatorContext) where TUpdate : class
+        {
+            var update = navigatorContext.GetOriginalUpdateOrDefault<TUpdate>();
+        
+            return update ?? throw new Exception("Update was not found.");
+        }
+        
         public static TUpdate? GetOriginalUpdateOrDefault<TUpdate>(this INavigatorContext navigatorContext) where TUpdate : class
         {
             var update = navigatorContext.Extensions.GetValueOrDefault(OriginalUpdateContextExtension.OriginalUpdateKey);
