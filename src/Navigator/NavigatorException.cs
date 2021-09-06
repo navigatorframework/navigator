66572a5485008ea013c47e5957b86237c4a30af9
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-09-06 19:24:56 +0300

    feat: create NavigatorException

---
+++ src/Navigator/NavigatorException.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Navigator
+{
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public class NavigatorException : Exception
+    {
+        /// <inheritdoc />
+        public NavigatorException()
+        {
+        }
+
+        /// <inheritdoc />
+        protected NavigatorException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        /// <inheritdoc />
+        public NavigatorException(string? message) : base(message)
+        {
+        }
+
+        /// <inheritdoc />
+        public NavigatorException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
