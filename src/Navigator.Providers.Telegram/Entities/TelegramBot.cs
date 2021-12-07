2a6bdcca2972765fa609c5fb0004c901e27b1e7f
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-12-07 20:37:53 +0100

    feat: add more fields to TelegramBot

--- src/Navigator.Providers.Telegram/Entities/TelegramBot.cs
+++ src/Navigator.Providers.Telegram/Entities/TelegramBot.cs
@@ -1,3 +1,4 @@
+using Navigator.Context;
 using Navigator.Entities;
 
 namespace Navigator.Providers.Telegram.Entities;
@@ -13,4 +14,42 @@ public record TelegramBot(long ExternalIdentifier) : Bot(ExternalIdentifier.ToSt
     /// Username of the bot.
     /// </summary>
     public string Username { get; init; }
+
+    /// <summary>
+    /// First name of the bot.
+    /// </summary>
+    public string FirstName { get; init; } = default!;
+    
+    /// <summary>
+    /// Last name of the bot.
+    /// <remarks>
+    ///     Optional.
+    /// </remarks>
+    /// </summary>
+    public string? LastName { get; init; }
+    
+    /// <summary>
+    /// Whether the bot can join groups or not.
+    /// <remarks>
+    ///     Optional. Only available on <see cref="NavigatorContext.BotProfile"/>
+    /// </remarks>
+    /// </summary>
+    public bool? CanJoinGroups { get; set; }
+
+    /// <summary>
+    /// Whether the bot can read all group messages or not.
+    /// <remarks>
+    ///     Optional. Only available on <see cref="NavigatorContext.BotProfile"/>
+    /// </remarks>
+    /// </summary>
+    public bool? CanReadAllGroupMessages { get; set; }
+
+    /// <summary>
+    /// Whether the bot supports inline queries or not.
+    /// <remarks>
+    ///     Optional. Only available on <see cref="NavigatorContext.BotProfile"/>
+    /// </remarks>
+    /// </summary>
+    public bool? SupportsInlineQueries { get; set; }
+
 }
