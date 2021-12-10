0a95639bde12387be6eb900282f3b96184b5b3ef
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-12-10 15:13:22 +0100

    feat: add INavigatorContextBuilderConversationSource
    This interface defines a source to use while building a NavigatorContext for filling Conversation parameters

---
+++ src/Navigator/Context/INavigatorContextBuilderConversationSource.cs
@@ -0,0 +1,18 @@
+using System.Threading.Tasks;
+using Navigator.Entities;
+
+namespace Navigator.Context;
+
+/// <summary>
+/// Defines a <see cref="GetConversationAsync"/> source for <see cref="INavigatorContextBuilder"/> to use
+/// while building a <see cref="NavigatorContext"/>.
+/// </summary>
+public interface INavigatorContextBuilderConversationSource
+{
+    /// <summary>
+    /// Returns a <see cref="Conversation"/> in accordance to the original event that spawned this context.
+    /// </summary>
+    /// <param name="originalEvent"></param>
+    /// <returns><see cref="Conversation"/></returns>
+    Task<Conversation> GetConversationAsync(object? originalEvent);
+}
