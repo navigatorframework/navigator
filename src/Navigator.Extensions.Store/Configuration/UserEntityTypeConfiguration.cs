10c6ea1796bc7604e9917437cb741766738011d5
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-12-10 17:05:16 +0100

    feat: add UserEntityTypeConfiguration

---
+++ src/Navigator.Extensions.Store/Configuration/UserEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Navigator.Extensions.Store.Entities;
+
+namespace Navigator.Extensions.Store.Configuration;
+
+public class UserEntityTypeConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.HasMany(e => e.ProviderEntities)
+            .WithOne();
+
+        builder.HasMany(e => e.Chats)
+            .WithMany(e => e.Users)
+            .UsingEntity<Conversation>(
+                e => e.HasOne(e => e.Chat)
+                    .WithMany(e => e.Conversations),
+                e=> e.HasOne(e => e.User)
+                    .WithMany(e => e.Conversations));
+
+        builder.Property(e => e.FirstInteractionAt)
+            .IsRequired();
+    }
+}
