51a7fa5f9cab3244ef60be65e472cff2bd4acc66
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-12-10 17:05:56 +0100

    feat: update NavigatorDbContext

--- src/Navigator.Extensions.Store/NavigatorDbContext.cs
+++ src/Navigator.Extensions.Store/NavigatorDbContext.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Navigator.Extensions.Store.Configuration;
+using Navigator.Extensions.Store.Entities;
+
 namespace Navigator.Extensions.Store;
 
 public class NavigatorDbContext : NavigatorDbContext<User, Chat>
@@ -6,14 +10,16 @@ public class NavigatorDbContext : NavigatorDbContext<User, Chat>
     {
     }
 }
-    
+
 public class NavigatorDbContext<TUser, TChat> : DbContext
     where TUser : User
     where TChat : Chat
 {
     public DbSet<TUser> Users { get; set; }
     public DbSet<TChat> Chats { get; set; }
-    public DbSet<Conversation> Conversations { get; set; }
+
+    protected DbSet<INavigatorProviderChatEntity> NavigatorProviderUserEntities { get; set; }
+    protected DbSet<INavigatorProviderChatEntity> NavigatorProviderChatEntities { get; set; }
 
     protected NavigatorDbContext()
     {
@@ -27,7 +33,8 @@ public class NavigatorDbContext<TUser, TChat> : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
+        modelBuilder.ApplyConfiguration(new ChatEntityTypeConfiguration());
+        modelBuilder.ApplyConfiguration(new UserEntityTypeConfiguration());
         modelBuilder.ApplyConfiguration(new ConversationEntityTypeConfiguration());
-
     }
 }
