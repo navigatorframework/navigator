544f146e0a61a3f25206d67ecb053045e53c77c1
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-12-10 17:04:46 +0100

    feat: add ChatEntityTypeConfiguration

---
+++ src/Navigator.Extensions.Store/Configuration/ChatEntityTypeConfiguration.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Navigator.Extensions.Store.Entities;
+
+namespace Navigator.Extensions.Store.Configuration;
+
+public class ChatEntityTypeConfiguration : IEntityTypeConfiguration<Chat>
+{
+    public void Configure(EntityTypeBuilder<Chat> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.HasMany(e => e.ProviderEntities)
+            .WithOne();
+
+        builder.HasMany(e => e.Users)
+            .WithMany(e => e.Chats)
+            .UsingEntity<Conversation>(
+                e => e.HasOne(e => e.User)
+                    .WithMany(e => e.Conversations),
+                e=> e.HasOne(e => e.Chat)
+                    .WithMany(e => e.Conversations));
+
+        builder.Property(e => e.FirstInteractionAt)
+            .IsRequired();
+    }
+}
