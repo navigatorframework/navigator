6b7c720aaea313687acc4224591787dd40ef10e4
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-12-23 11:52:50 +0100

    feat: add interaction with UniversalStore to EchoAction

--- src/Navigator.Samples.Store/Actions/EchoActionHandler.cs
+++ src/Navigator.Samples.Store/Actions/EchoActionHandler.cs
@@ -1,5 +1,8 @@
-﻿using System.Threading;
+﻿using System.Globalization;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Navigator.Actions;
 using Navigator.Context;
 using Navigator.Extensions.Store;
@@ -10,13 +13,18 @@ namespace Navigator.Samples.Store.Actions;
 
 public class EchoActionHandler : ActionHandler<EchoAction>
 {
-    public EchoActionHandler(INavigatorContextAccessor navigatorContextAccessor) : base(navigatorContextAccessor)
+    private readonly NavigatorDbContext _dbContext;
+    public EchoActionHandler(INavigatorContextAccessor navigatorContextAccessor, NavigatorDbContext dbContext) : base(navigatorContextAccessor)
     {
+        _dbContext = dbContext;
     }
 
     public override async Task<Status> Handle(EchoAction action, CancellationToken cancellationToken)
     {
-        var store = NavigatorContext.GetStoreOrDefault();
+        var uc = await NavigatorContext.GetStoreOrDefault()?.FindConversation(NavigatorContext.Conversation, NavigatorContext.Provider.Name, cancellationToken);
+        
+        await this.GetTelegramClient().SendTextMessageAsync(this.GetTelegramChat().Id, 
+             uc.FirstInteractionAt.ToLongDateString(), cancellationToken: cancellationToken);
         
         await this.GetTelegramClient().SendTextMessageAsync(this.GetTelegramChat().Id, 
             action.MessageToEcho, cancellationToken: cancellationToken);
