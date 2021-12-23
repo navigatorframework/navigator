77a190dfd9002f1cdd33892058b23278eec32ea2
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-12-23 11:30:46 +0100

    feat: add new methods to IUniversalStore

--- src/Navigator.Extensions.Store/IUniversalStore.cs
+++ src/Navigator.Extensions.Store/IUniversalStore.cs
@@ -1,13 +1,29 @@
+using Navigator.Entities;
+using Navigator.Extensions.Store.Entities;
+
 namespace Navigator.Extensions.Store;
 
 public interface IUniversalStore
 {
-    Task AddUserProfile(string provider, Guid identification, CancellationToken cancellationToken = default);
-    Task TryAddUserProfile(string provider, Guid identification, CancellationToken cancellationToken = default);
-    
-    Task AddChatProfile(string provider, Guid identification, CancellationToken cancellationToken = default);
-    Task TryAddChatProfile(string provider, Guid identification, CancellationToken cancellationToken = default);
+    #region Conversation
 
-    Task AddConversationProfile(string provider, Guid identification, CancellationToken cancellationToken = default);
-    Task TryAddConversationProfile(string provider, Guid identification, CancellationToken cancellationToken = default);
+    Task<UniversalConversation> FindOrCreateConversation(Conversation conversation, string provider,
+        CancellationToken cancellationToken = default);
+    Task<UniversalConversation?> FindConversation(Conversation conversation, string provider,
+        CancellationToken cancellationToken = default);
+    
+    #endregion
+    
+    #region Profile
+
+    Task AddUserProfile(UniversalUser user, string provider, Guid identification, CancellationToken cancellationToken = default, bool? saveChanges = true);
+    Task TryAddUserProfile(UniversalUser user, string provider, Guid identification, CancellationToken cancellationToken = default, bool? saveChanges = true);
+    
+    Task AddChatProfile(UniversalChat chat, string provider, Guid identification, CancellationToken cancellationToken = default, bool? saveChanges = true);
+    Task TryAddChatProfile(UniversalChat chat, string provider, Guid identification, CancellationToken cancellationToken = default, bool? saveChanges = true);
+
+    Task AddConversationProfile(UniversalConversation conversation, string provider, Guid identification, CancellationToken cancellationToken = default, bool? saveChanges = true);
+    Task TryAddConversationProfile(UniversalConversation conversation, string provider, Guid identification, CancellationToken cancellationToken = default, bool? saveChanges = true);
+
+    #endregion
 }
