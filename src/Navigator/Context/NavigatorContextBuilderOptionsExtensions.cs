70592a4c90ff3157fd2d3cd01ef450e5c394cf9d
Lucas Maximiliano Marino <hello@lucasmarino.me>
2023-10-07 00:09:49 +0200

    refactor: delete unused context builder option

--- src/Navigator/Context/NavigatorContextBuilderOptionsExtensions.cs
+++ src/Navigator/Context/NavigatorContextBuilderOptionsExtensions.cs
@@ -2,23 +2,6 @@ namespace Navigator.Context;
 
 public static class NavigatorContextBuilderOptionsExtensions
 {
-    #region Provider
-
-    private const string ProviderKey = "_navigator.context.options.provider";
-
-    public static void SetProvider<TProvider>(this INavigatorContextBuilderOptions contextBuilderOptions) where TProvider : INavigatorProvider
-    {
-        contextBuilderOptions.TryRegisterOption(ProviderKey, typeof(TProvider));
-
-    }
-
-    public static Type? GetProvider(this INavigatorContextBuilderOptions contextBuilderOptions)
-    {
-        return contextBuilderOptions.RetrieveOption<Type>(ProviderKey);
-    }
-        
-    #endregion
-        
     #region ActionType
 
     private const string ActionTypeKey = "_navigator.context.options.action_type";
