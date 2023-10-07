1e4f593ce17477f57dd8cb441736d82128c72923
Lucas Maximiliano Marino <hello@lucasmarino.me>
2023-10-07 12:15:04 +0200

    refactor: move route configuration to base

--- src/Navigator/Configuration/EndpointRouteBuilderExtensions.cs
+++ src/Navigator/Configuration/EndpointRouteBuilderExtensions.cs
@@ -1,4 +1,9 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Telegram.Bot.Types;
 
 namespace Navigator.Configuration;
 
@@ -9,8 +14,45 @@ public static class EndpointRouteBuilderExtensions
     /// </summary>
     /// <param name="endpointRouteBuilder"></param>
     /// <returns></returns>
-    public static NavigatorRouteConfiguration MapNavigator(this IEndpointRouteBuilder endpointRouteBuilder)
+    public static void MapNavigator(this IEndpointRouteBuilder endpointRouteBuilder)
     {
-        return new(endpointRouteBuilder);
+        using var scope = endpointRouteBuilder.ServiceProvider.CreateScope();
+
+        var options = scope.ServiceProvider.GetRequiredService<NavigatorOptions>();
+
+        endpointRouteBuilder.MapPost(options.GetWebHookEndpointOrDefault(), ProcessTelegramUpdate);
+    }
+        
+    private static async Task ProcessTelegramUpdate(HttpContext context)
+    {
+        context.Response.StatusCode = 200;
+
+        if (context.Request.ContentType != "application/json")
+        {
+            return;
+        }
+
+        var telegramUpdate = await ParseTelegramUpdate(context.Request.Body);
+
+        if (telegramUpdate is not null)
+        {
+            var navigatorMiddleware = context.RequestServices.GetRequiredService<TelegramMiddleware>();
+
+            await navigatorMiddleware.Process(telegramUpdate);
+        }
+    }
+        
+    private static async Task<Update?> ParseTelegramUpdate(Stream stream)
+    {
+        try
+        {
+            var update = await JsonSerializer.DeserializeAsync<Update>(stream);
+
+            return update?.Id == default ? default : update;
+        }
+        catch
+        {
+            return default;
+        }
     }
 }
