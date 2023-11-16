1f0a2077f33c84d82261dfa34c4cc4d7f4139e7b
Lucas Maximiliano Marino <hello@lucasmarino.me>
2023-11-16 23:19:34 +0100

    docs: add summary for Status.cs

--- src/Navigator/Actions/Status.cs
+++ src/Navigator/Actions/Status.cs
@@ -1,12 +1,26 @@
 namespace Navigator.Actions;
 
+/// <summary>
+/// Represents the outcome of an action.
+/// </summary>
 public readonly struct Status
 {
     private readonly bool _isSuccess;
 
+    /// <summary>
+    /// Is success.
+    /// </summary>
     public bool IsSuccess => _isSuccess;
+    
+    /// <summary>
+    /// Is error.
+    /// </summary>
     public bool IsError => !_isSuccess;
 
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    /// <param name="isSuccess"></param>
     public Status(bool isSuccess)
     {
         _isSuccess = isSuccess;
