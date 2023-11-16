4ec3d3b9c4c962ceb5400bd11dc4a91bf68eea8f
Lucas Maximiliano Marino <hello@lucasmarino.me>
2023-11-16 23:11:31 +0100

    refactor: make Success and Error protected functions. Also add summaries.

--- src/Navigator/Actions/ActionHandler.cs
+++ src/Navigator/Actions/ActionHandler.cs
@@ -4,24 +4,49 @@ using Navigator.Context.Accessor;
 
 namespace Navigator.Actions;
 
+/// <summary>
+/// Implement to handle an action.
+/// </summary>
+/// <typeparam name="TAction"></typeparam>
 public abstract class ActionHandler<TAction> : IRequestHandler<TAction, Status> where TAction : IAction
 {
+    /// <summary>
+    /// Context for the action.
+    /// </summary>
     public readonly INavigatorContext Context;
 
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    /// <param name="navigatorContextAccessor"></param>
     protected ActionHandler(INavigatorContextAccessor navigatorContextAccessor)
     {
         Context = navigatorContextAccessor.NavigatorContext;
     }
 
+    /// <summary>
+    /// Handles the action.
+    /// </summary>
+    /// <param name="action"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns><see cref="Status"/></returns>
     public abstract Task<Status> Handle(TAction action, CancellationToken cancellationToken);
 
-    public static Status Success()
+    /// <summary>
+    /// Called when the action was handled successfully.
+    /// </summary>
+    /// <returns><see cref="Status"/></returns>
+    protected static Status Success()
     {
-        return new(true);
+        return new Status(true);
     }
 
-    public static Status Error()
+    /// <summary>
+    /// Called when the action was not handled successfully.
+    /// </summary>
+    /// <returns><see cref="Status"/></returns>
+    protected static Status Error()
     {
-        return new(false);
+        return new Status(false);
     }
 }
