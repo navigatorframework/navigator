b0eedc26c841da7f7634e9db0f57fdf13e962711
Lucas Maximiliano Marino <hello@lucasmarino.me>
2023-11-17 13:35:51 +0100

    docs: update summary for INavigatorContextBuilder.cs

--- src/Navigator/Context/Builder/INavigatorContextBuilder.cs
+++ src/Navigator/Context/Builder/INavigatorContextBuilder.cs
@@ -2,7 +2,15 @@ using Navigator.Context.Builder.Options;
 
 namespace Navigator.Context.Builder;
 
+/// <summary>
+/// Navigator Context Builder.
+/// </summary>
 public interface INavigatorContextBuilder
 {
+    /// <summary>
+    /// Builds a <see cref="NavigatorContext"/> and returns it.
+    /// </summary>
+    /// <param name="configurationAction"></param>
+    /// <returns></returns>
     Task<INavigatorContext> Build(Action<INavigatorContextBuilderOptions> configurationAction);
 }
