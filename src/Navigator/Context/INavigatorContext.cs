d9604f3d59893e8295c44e3fa09f468282a1a5c0
Lucas Maximiliano Marino <hello@lucasmarino.me>
2023-11-17 13:30:46 +0100

    docs: update summaries for INavigatorContext.cs and INavigatorContextFactory.cs

--- src/Navigator/Context/INavigatorContext.cs
+++ src/Navigator/Context/INavigatorContext.cs
@@ -5,10 +5,33 @@ namespace Navigator.Context;
 
 public interface INavigatorContext
 {
+    /// <summary>
+    /// Client to use for interacting with Navigator and the provider (Telegram).
+    /// </summary>
     public INavigatorClient Client { get; }
+    
+    /// <summary>
+    /// Profile of the bot in <see cref="Bot"/> form.
+    /// </summary>
     Bot BotProfile { get; }
+    
+    /// <summary>
+    /// Installed extensions for <see cref="INavigatorContext"/>
+    /// </summary>
     Dictionary<string, object?> Extensions { get; }
+    
+    /// <summary>
+    /// Useful items stored here.
+    /// </summary>
     Dictionary<string, string> Items { get; }
+    
+    /// <summary>
+    /// Type of the action for this context.
+    /// </summary>
     public string ActionType { get; }
+    
+    /// <summary>
+    /// Conversation related to this context.
+    /// </summary>
     public Conversation Conversation { get; }
 }
