0335bbafc335d0a77464f3e70ce8f8cb0d119bb0
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-07-31 12:42:58 +0200

    feat: add more methods to IBotActionCatalogBuilder

--- src/Navigator/Builder/IBotActionCatalogBuilder.cs
+++ src/Navigator/Builder/IBotActionCatalogBuilder.cs
@@ -3,12 +3,16 @@ using Telegram.Bot.Types;
 
 namespace Navigator.Builder;
 
-public interface IBotStrategyBuilder
+public interface IBotActionCatalogBuilder
 {
     public IBotActionBuilder OnUpdate(Func<Update, Task<bool>> condition, Func<INavigatorContext, Task> handler);
     public IBotActionBuilder OnUpdate(Func<Update, Task<bool>> condition, Action<INavigatorContext> handler);
     public IBotActionBuilder OnUpdate(Func<Update, bool> condition, Func<INavigatorContext, Task> handler);
     public IBotActionBuilder OnUpdate(Func<Update, bool> condition, Action<INavigatorContext> handler);
+    public IBotActionBuilder OnUpdate(Delegate condition, Delegate handler);
+    public IBotActionBuilder OnUpdate(Func<Update, Task<bool>> condition, Delegate handler);
+    public IBotActionBuilder OnUpdate(Func<Update, bool> condition, Delegate handler);
+
     
     // public NavigatorBotBuilder OnCommand(string command, Func<INavigatorContext, string[], Task> action);
 }
