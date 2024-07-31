710363875f7d929fb3a63024770ea5be89f0928c
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-07-31 12:41:25 +0200

    feat: experiment with delegate execution

--- src/Navigator/Actions/BotAction.cs
+++ src/Navigator/Actions/BotAction.cs
@@ -5,53 +5,38 @@ namespace Navigator.Actions;
 
 public record BotAction
 {
-    private Func<Update, Task<bool>>? ConditionAsync { get; } = default;
-    private Func<Update, bool>? Condition { get; } = default;
-
-    private Func<INavigatorContext, Task>? HandlerAsync { get; } = default;
-    private Action<INavigatorContext>? Handler { get; } = default;
+    // private Func<Update, Task<bool>>? ConditionAsync { get; } = default;
+    // private Func<Update, bool>? Condition { get; } = default;
+    //
+    // private Func<INavigatorContext, Task>? HandlerAsync { get; } = default;
+    // private Action<INavigatorContext>? Handler { get; } = default;
     
-    /// <summary>
-    /// Constructor for condition and handler being both async delegates.
-    /// </summary>
-    /// <param name="condition"></param>
-    /// <param name="handler"></param>
-    public BotAction(Func<Update, Task<bool>> condition, Func<INavigatorContext, Task> handler)
-    {
-        ConditionAsync = condition;
-        HandlerAsync = handler;
-    }
+    private static Delegate Condition { get; set; }
+    private static Delegate Handler { get; set; }
 
-    /// <summary>
-    /// Constructor for an async condition delegate with a sync handler delegate.
-    /// </summary>
-    /// <param name="condition"></param>
-    /// <param name="handler"></param>
-    public BotAction(Func<Update, Task<bool>> condition, Action<INavigatorContext> handler)
-    {
-        ConditionAsync = condition;
-        Handler = handler;
-    }
-
-    /// <summary>
-    /// Constructor for a sync condition delegate with an async handler delegate.
-    /// </summary>
-    /// <param name="condition"></param>
-    /// <param name="handler"></param>
-    public BotAction(Func<Update, bool> condition, Func<INavigatorContext, Task> handler)
-    {
-        Condition = condition;
-        HandlerAsync = handler;
-    }
-
-    /// <summary>
-    /// Constructor for condition and handler being both sync delegates.
-    /// </summary>
-    /// <param name="condition"></param>
-    /// <param name="handler"></param>
-    public BotAction(Func<Update, bool> condition, Action<INavigatorContext> handler)
+    public BotAction(Delegate condition, Delegate handler)
     {
         Condition = condition;
         Handler = handler;
+    }
+    
+    public async Task ExecuteCondition(params object[] args)
+    {
+        var result = Handler.DynamicInvoke(args);
+
+        if (result is Task task)
+        {
+            await task;
+        }
+    }
+
+    public async Task ExecuteHandler(params object[] args)
+    {
+        var result = Handler.DynamicInvoke(args);
+
+        if (result is Task task)
+        {
+            await task;
+        }
     }
 }
