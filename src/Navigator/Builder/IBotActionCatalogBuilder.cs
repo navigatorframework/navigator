efad91ade61733c0c6e3c5fa2062b7ecbb10be0a
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-07-31 13:14:35 +0200

    feat: add BotAction related properties to IBotActionCatalogBuilder as public

--- src/Navigator/Builder/IBotActionCatalogBuilder.cs
+++ src/Navigator/Builder/IBotActionCatalogBuilder.cs
@@ -1,10 +1,17 @@
+using Navigator.Actions;
 using Navigator.Context;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Navigator.Builder;
 
 public interface IBotActionCatalogBuilder
 {
+    public Dictionary<Guid, BotAction> Actions { get; }
+    public Dictionary<Guid, IEnumerable<Type>> ConditionInputTypesByAction { get; }
+    public Dictionary<Guid, IEnumerable<Type>> HandlerInputTypesByAction { get; }
+    public Dictionary<Guid, string> ActionTypeByAction { get; }
+    
     public IBotActionBuilder OnUpdate(Delegate condition, Delegate handler);
     // public IBotActionBuilder OnUpdate(Func<Update, Task<bool>> condition, Delegate handler);
     // public IBotActionBuilder OnUpdate(Func<Update, Task<bool>> condition, Action<INavigatorContext> handler);
