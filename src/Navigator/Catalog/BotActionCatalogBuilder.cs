e867582820e0c6715a839a67c373cb8efc678a6c
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-09 12:20:00 +0200

    feat: implement BotActionCatalogBuilder

---
+++ src/Navigator/Catalog/BotActionCatalogBuilder.cs
@@ -0,0 +1,29 @@
+using System.Collections.ObjectModel;
+using Navigator.Actions;
+using Navigator.Actions.Builder;
+
+namespace Navigator.Catalog;
+
+public class BotActionCatalogBuilder : IBotActionCatalogBuilder
+{
+    public List<BotActionBuilder> Actions { get; } = [];
+
+    public IBotActionBuilder OnUpdate(Delegate condition, Delegate handler)
+    {
+        var id = Guid.NewGuid();
+        var actionBuilder = new BotActionBuilder(condition, handler);
+
+        Actions.Add(actionBuilder);
+
+        return actionBuilder;
+    }
+
+    public BotActionCatalog Build()
+    {
+        var actions = Actions
+            .Select(actionBuilder => actionBuilder.Build())
+            .ToList();
+        
+        return new BotActionCatalog(actions);
+    }
+}
