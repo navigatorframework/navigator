df896207d1a0b0262397d3f99384e78276e243f5
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-19 14:25:44 +0200

    feat: update NavigatorStrategy extract method for argument resolution, add documentation

--- src/Navigator/Strategy/NavigatorStrategy.cs
+++ src/Navigator/Strategy/NavigatorStrategy.cs
@@ -6,15 +6,29 @@ using Navigator.Entities;
 using Navigator.Strategy.Classifier;
 using Navigator.Telegram;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Chat = Navigator.Entities.Chat;
 
 namespace Navigator.Strategy;
 
+/// <summary>
+/// Implements a strategy for dynamic decision-making based on incoming updates. This strategy involves classifying updates,
+/// selecting relevant actions from a catalog, and executing those actions asynchronously.
+/// <seealso cref="BotActionCatalog"/>
+/// <seealso cref="IUpdateClassifier"/>
+/// </summary>
 public class NavigatorStrategy : INavigatorStrategy
 {
     private readonly BotActionCatalog _catalog;
     private readonly IUpdateClassifier _classifier;
     private readonly IServiceProvider _serviceProvider;
 
+    /// <summary>
+    /// Initializes a new instance of <see cref="NavigatorStrategy"/>.
+    /// </summary>
+    /// <param name="catalogFactory">An instance of <see cref="IBotActionCatalogFactory"/>.</param>
+    /// <param name="classifier">An instance of <see cref="IUpdateClassifier"/>.</param>
+    /// <param name="serviceProvider">An instance of <see cref="IServiceProvider"/></param>.
     public NavigatorStrategy(IBotActionCatalogFactory catalogFactory, IUpdateClassifier classifier,
         IServiceProvider serviceProvider)
     {
@@ -23,6 +37,13 @@ public class NavigatorStrategy : INavigatorStrategy
         _serviceProvider = serviceProvider;
     }
 
+    /// <summary>
+    /// Processes an <see cref="Update"/> by determining the appropriate action type,
+    /// retrieving relevant <see cref="BotAction"/> from the <see cref="BotActionCatalog"/>,
+    /// filtering those actions based on the <see cref="Update"/>,
+    /// and executing each filtered action asynchronously.
+    /// </summary>
+    /// <param name="update">The <see cref="Update"/> object to be processed.</param>
     public async Task Invoke(Update update)
     {
         var actionType = await _classifier.Process(update);
@@ -46,19 +67,8 @@ public class NavigatorStrategy : INavigatorStrategy
 
             foreach (var inputType in action.Information.ConditionInputTypes)
             {
-                //TODO: extract method
-                arguments.Add(inputType switch
-                {
-                    not null when inputType == typeof(Update)
-                        => update,
-                    not null when inputType == typeof(Conversation) 
-                        => update.GetConversation(),
-                    not null when inputType == typeof(Bot) 
-                        => await _serviceProvider.GetRequiredService<INavigatorClient>().GetProfile(),
-                    not null => _serviceProvider.GetRequiredService(inputType),
-                    //TODO: this exception should never happen.
-                    _ => throw new NavigatorException()
-                });
+                var argument = await GetArgument(inputType, update, action);
+                arguments.Add(argument);
             }
 
             if (await action.ExecuteCondition(arguments))
@@ -76,20 +86,33 @@ public class NavigatorStrategy : INavigatorStrategy
 
         foreach (var inputType in action.Information.HandlerInputTypes)
         {
-            arguments.Add(inputType switch
-            {
-                not null when inputType == typeof(Update)
-                    => update,
-                not null when inputType == typeof(Conversation) 
-                    => update.GetConversation(),
-                not null when inputType == typeof(Bot) 
-                    => await _serviceProvider.GetRequiredService<INavigatorClient>().GetProfile(),
-                not null => _serviceProvider.GetRequiredService(inputType),
-                //TODO: this exception should never happen.
-                _ => throw new NavigatorException()
-            });
+            var argument = await GetArgument(inputType, update, action);
+            arguments.Add(argument);
         }
 
         await action.ExecuteHandler(arguments);
+    }
+    
+    private async Task<object> GetArgument(Type inputType, Update update, BotAction action)
+    {
+        var argument = inputType switch
+        {
+            not null when inputType == typeof(Update)
+                => update,
+            not null when inputType == typeof(Conversation) 
+                => update.GetConversation(),
+            not null when inputType == typeof(Chat) 
+                => update.GetChatOrDefault()!,
+            not null when inputType == typeof(Conversation) 
+                => update.GetUserOrDefault()!,
+            not null when inputType == typeof(Bot) 
+                => await _serviceProvider.GetRequiredService<INavigatorClient>().GetProfile(),
+            not null when inputType == typeof(string[]) && action.Information.ActionType == $"{typeof(MessageType)}.{nameof(MessageEntityType.BotCommand)}"
+                => update.Message!.ExtractArguments(),
+            not null => _serviceProvider.GetRequiredService(inputType),
+            //TODO: this exception should never happen.
+            _ => throw new NavigatorException()
+        };
+        return argument;
     }
 }
