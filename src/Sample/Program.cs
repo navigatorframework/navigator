f75d22e85da591467184c3bd7df20b1f3653cd28
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-20 12:11:06 +0200

    feat: update sample

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -16,28 +16,27 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         builder.Configuration.AddCommonConfiguration();
-        
+
         builder.Services.AddNavigator(options =>
         {
             options.SetWebHookBaseUrl(builder.Configuration["BASE_WEBHOOK_URL"]!);
             options.SetTelegramToken(builder.Configuration["TELEGRAM_TOKEN"]!);
             options.EnableTypingNotification();
         });
-        
+
         var app = builder.Build();
 
         var bot = app.GetBot();
 
         bot.OnCommand("join", async (INavigatorClient client, Chat chat, string[] parameters) =>
         {
-            var result = string.Join(' ', parameters);
-        
+            var result = string.Join(',', parameters);
+
             await client.SendTextMessageAsync(chat, result);
         });
-        
+
         app.MapNavigator();
-        
+
         app.Run();
     }
 }
-
