5958ea5e157944910908f5745676490ac02a898b
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-20 16:15:33 +0200

    refactor: convert Program to top level code

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -9,43 +9,35 @@ using Telegram.Bot;
 using Telegram.Bot.Types;
 using Chat = Navigator.Entities.Chat;
 
-namespace Sample;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Configuration.AddCommonConfiguration();
+
+builder.Services.AddNavigator(options =>
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+    options.SetWebHookBaseUrl(builder.Configuration["BASE_WEBHOOK_URL"]!);
+    options.SetTelegramToken(builder.Configuration["TELEGRAM_TOKEN"]!);
+    options.EnableTypingNotification();
+    options.EnableMultipleActionsUsage();
+});
 
-        builder.Configuration.AddCommonConfiguration();
+var app = builder.Build();
 
-        builder.Services.AddNavigator(options =>
-        {
-            options.SetWebHookBaseUrl(builder.Configuration["BASE_WEBHOOK_URL"]!);
-            options.SetTelegramToken(builder.Configuration["TELEGRAM_TOKEN"]!);
-            options.EnableTypingNotification();
-            options.EnableMultipleActionsUsage();
-        });
+var bot = app.GetBot();
 
-        var app = builder.Build();
+// This action will be triggered if the user sends a message in the style of `/join <text>`.
+bot.OnCommand("join", async (INavigatorClient client, Chat chat, string[] parameters) =>
+{
+    var result = string.Join(',', parameters);
 
-        var bot = app.GetBot();
+    await client.SendTextMessageAsync(chat, result);
+});
 
-        // This action will be triggered if the user sends a message in the style of `/join <text>`.
-        bot.OnCommand("join", async (INavigatorClient client, Chat chat, string[] parameters) =>
-        {
-            var result = string.Join(',', parameters);
+// This action will be triggered for every message sent to the chat. Additionally in this code example, this action will be triggered
+// only if NavigatorOptions.MultipleActionsUSageIsEnabled is set to true.
+bot.OnMessage((Update _) => true,
+    async (INavigatorClient client, Chat chat) => { await client.SendTextMessageAsync(chat, "text received!"); });
 
-            await client.SendTextMessageAsync(chat, result);
-        });
+app.MapNavigator();
 
-        // This action will be triggered for every message sent to the chat. Additionally in this code example, this action will be triggered
-        // only if NavigatorOptions.MultipleActionsUSageIsEnabled is set to true.
-        bot.OnMessage((Update _) => true,
-            async (INavigatorClient client, Chat chat) => { await client.SendTextMessageAsync(chat, "text received!"); });
-
-        app.MapNavigator();
-
-        app.Run();
-    }
-}
+app.Run();
