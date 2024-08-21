f948facd63a76b8652a011596057b3e8b1ef2374
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-21 16:21:51 +0200

    chore: update sample and README.md

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -1,5 +1,7 @@
+using System;
 using Incremental.Common.Configuration;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Navigator;
 using Navigator.Catalog.Factory.Extensions;
 using Navigator.Client;
@@ -12,6 +14,8 @@ using Chat = Navigator.Entities.Chat;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddCommonConfiguration();
+
+builder.Services.AddMemoryCache();
 
 builder.Services.AddNavigator(options =>
 {
@@ -36,11 +40,19 @@ bot.OnCommand("join", async (INavigatorClient client, Chat chat, string[] parame
 // This action will be triggered for every message sent to the chat. Additionally in this code example, this action will be triggered
 // only if NavigatorOptions.MultipleActionsUSageIsEnabled is set to true.
 bot.OnMessage((Update _) => true, async (INavigatorClient client, Chat chat, Message message) =>
-{
-    var text = $"message received: {message.MessageId}";
+    {
+        var text = $"message received: {message.MessageId}";
 
-    await client.SendTextMessageAsync(chat, text);
-});
+        await client.SendTextMessageAsync(chat, text);
+    })
+    .WithCooldown(TimeSpan.FromSeconds(30));
+
+// This action will be triggered if the user sends a message in the style of `/sad`.
+bot.OnCommand("sad",
+    async (INavigatorClient client, Chat chat) =>
+    {
+        await client.SendStickerAsync(chat, "CAACAgQAAxkBAAI5DF59uqkJYnqzc5LcnEC_bdp0rerIAAJsAwACmOejAAG_qYNUT_L_exgE");
+    });
 
 app.MapNavigator();
 
