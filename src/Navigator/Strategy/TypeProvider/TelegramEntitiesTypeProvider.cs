d4934d9243d75aed4fa0114cf1dc26d93e5ce33a
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-08-21 12:49:36 +0200

    feat: add implementation of IArgumentTypeProvider for Telegram entities

---
+++ src/Navigator/Strategy/TypeProvider/TelegramEntitiesTypeProvider.cs
@@ -0,0 +1,32 @@
+using Navigator.Actions;
+using Navigator.Telegram;
+using Telegram.Bot.Types;
+
+namespace Navigator.Strategy.TypeProvider;
+
+internal sealed record TelegramEntitiesTypeProvider : IArgumentTypeProvider
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public TelegramEntitiesTypeProvider(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    /// <inheritdoc />
+    public ushort Priority { get; } = 10000;
+
+    public ValueTask<object?> GetArgument(Type type, Update update, BotAction action)
+    {
+        return ValueTask.FromResult<object?>(type switch
+        {
+            not null when type == typeof(Update)
+                => update,
+            not null when type == typeof(Chat)
+                => update.GetChatOrDefault(),
+            not null when type == typeof(User)
+                => update.GetUserOrDefault(),
+            _ => default
+        });
+    }
+}
