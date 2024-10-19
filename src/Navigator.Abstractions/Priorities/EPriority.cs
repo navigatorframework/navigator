7d3cf6116c296ee9a4b8bb8dfa8cb40d87d0aa44
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-19 12:59:28 +0200

    feat: add attribute to use EPriority

--- src/Navigator.Abstractions/Priorities/EPriority.cs
+++ src/Navigator.Abstractions/Priorities/EPriority.cs
@@ -1,7 +1,7 @@
-namespace Navigator.Abstractions.Priority;
+namespace Navigator.Abstractions.Priorities;
 
 /// <summary>
-///     Priority presets.
+///     Priorities presets.
 /// </summary>
 public enum EPriority
 {
@@ -9,42 +9,42 @@ public enum EPriority
     ///     Highest priority.
     /// </summary>
     Highest = 1,
-    
+
     /// <summary>
     ///     Very high priority.
     /// </summary>
     VeryHigh = 2,
-    
+
     /// <summary>
     ///     High priority.
     /// </summary>
     High = 3,
-    
+
     /// <summary>
     ///     Above normal priority.
     /// </summary>
     AboveNormal = 4,
-    
+
     /// <summary>
     ///     Normal priority.
     /// </summary>
     Normal = 5,
-    
+
     /// <summary>
     ///     Below normal priority.
     /// </summary>
     BelowNormal = 6,
-    
+
     /// <summary>
     ///     Low priority.
     /// </summary>
     Low = 7,
-    
+
     /// <summary>
     ///     Very low priority.
     /// </summary>
     VeryLow = 8,
-    
+
     /// <summary>
     ///     Lowest priority.
     /// </summary>
