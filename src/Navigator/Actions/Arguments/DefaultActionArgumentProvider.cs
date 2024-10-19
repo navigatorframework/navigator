cd8a6d5a7c5899459b7081d92bad43c48570dde7
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-19 12:31:36 +0200

    feat: add default implementation for IActionArgumentProvider

---
+++ src/Navigator/Actions/Arguments/DefaultActionArgumentProvider.cs
@@ -0,0 +1,39 @@
+using Microsoft.Extensions.Logging;
+using Navigator.Abstractions.Actions;
+using Navigator.Abstractions.Actions.Arguments;
+using Telegram.Bot.Types;
+
+namespace Navigator.Actions.Arguments;
+
+public class DefaultActionArgumentProvider : IActionArgumentProvider
+{
+    private readonly ILogger<DefaultActionArgumentProvider> _logger;
+    private readonly IArgumentResolver[] _resolvers;
+    private readonly IServiceProvider _serviceProvider;
+
+    public DefaultActionArgumentProvider(ILogger<DefaultActionArgumentProvider> logger, IArgumentResolver[] resolvers,
+        IServiceProvider serviceProvider)
+    {
+        _logger = logger;
+        _resolvers = resolvers;
+        _serviceProvider = serviceProvider;
+    }
+
+    /// <inheritdoc />
+    public async ValueTask<object?> GetArgument(Type inputType, Update update, BotAction action)
+    {
+        _logger.LogDebug("Resolving argument of type {InputType} for action {ActionName} in update {UpdateId}", inputType,
+            action.Information.Name, update.Id);
+
+        var argument = default(object?);
+
+        foreach (var resolver in _resolvers)
+        {
+            argument = await resolver.GetArgument(inputType, update, action);
+
+            if (argument is not null) break;
+        }
+
+        return argument ?? _serviceProvider.GetService(inputType);
+    }
+}
