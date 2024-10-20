695ee909adfe7d0d07a73925d8a53b3a07c92b85
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-20 21:41:31 +0200

    feat: repurpose NavigatorConfiguration for registering NavigatorOptions and Extensions

--- src/Navigator/Configuration/NavigatorConfiguration.cs
+++ src/Navigator/Configuration/NavigatorConfiguration.cs
@@ -1,50 +1,31 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
+using Navigator.Abstractions.Extensions;
 using Navigator.Configuration.Options;
 
 namespace Navigator.Configuration;
 
 /// <summary>
-/// Helper functions for configuring navigator services.
+///     Helper functions for configuring navigator services.
 /// </summary>
 public class NavigatorConfiguration
 {
-    /// <summary>
-    /// Gets the <see cref="NavigatorOptions"/> that are being used.
-    /// </summary>
-    /// <value>
-    /// The <see cref="NavigatorOptions"/> 
-    /// </value>
-    public NavigatorOptions Options { get; internal set; }
-        
-    /// <summary>
-    /// Gets the <see cref="IServiceCollection"/> services are attached to.
-    /// </summary>
-    /// <value>
-    /// The <see cref="IServiceCollection"/> services are attached to.
-    /// </value>
-    public IServiceCollection Services { get; internal set; }
+    private readonly List<(Type, object?)> _extensions = [];
 
     /// <summary>
-    /// Creates a new instance of <see cref="NavigatorConfiguration"/>.
+    ///     Gets the <see cref="NavigatorOptions" /> that are being used.
     /// </summary>
-    /// <param name="options">The <see cref="NavigatorOptions"/> to use.</param>
-    /// <param name="services">The <see cref="IServiceCollection"/> to attach to.</param>
-    public NavigatorConfiguration(Action<NavigatorOptions> options, IServiceCollection services)
+    /// <value>
+    ///     The <see cref="NavigatorOptions" />
+    /// </value>
+    public readonly NavigatorOptions Options;
+
+    public NavigatorConfiguration()
     {
         Options = new NavigatorOptions();
-        options.Invoke(Options);
-            
-        Services = services;
-
-        services.AddSingleton(Options);
     }
-    
-    /// <summary>
-    /// Registers the <see cref="NavigatorOptions"/> or replaces it if already exists.
-    /// </summary>
-    public void RegisterOrReplaceOptions()
+
+    public void WithExtension<TExtension>(Action<INavigatorExtensionOptions<TExtension>>? options = default)
+        where TExtension : INavigatorExtension
     {
-        Services.Replace(ServiceDescriptor.Singleton<NavigatorOptions, NavigatorOptions>(_ => Options));
+        _extensions.Add((typeof(TExtension), options));
     }
 }
