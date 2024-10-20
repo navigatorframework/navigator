2d8baf54efb290baadfa621ec147ad41a85ddcb3
Lucas Maximiliano Marino <hello@lucasmarino.me>
2024-10-20 21:43:17 +0200

    feat: define ProbabilitiesExtension

---
+++ src/Navigator.Extensions.Probabilities/Configuration/ProbabilitiesExtension.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.DependencyInjection;
+using Navigator.Abstractions.Actions;
+using Navigator.Abstractions.Extensions;
+using Navigator.Abstractions.Pipelines.Steps;
+using Navigator.Configuration.Options;
+using Navigator.Extensions.Probabilities.Step;
+
+namespace Navigator.Extensions.Probabilities.Configuration;
+
+/// <summary>
+///     Extension for adding probabilities of being executed to a <see cref="BotAction" />.
+/// </summary>
+public class ProbabilitiesExtension : INavigatorExtension
+{
+    public void Configure(IServiceCollection services, NavigatorOptions options)
+    {
+        services.AddSingleton<INavigatorPipelineStep, FilterByProbabilitiesStep>();
+    }
+}
