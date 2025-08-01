f482dce1124627345a6a5e53f0ba212dfc43a746
Lucas Maximiliano Marino <hello@lucasmarino.me>
2025-08-01 11:33:13 +0200

    refactor: simplify INavigatorExtensionOptions interface by removing redundant constraints

--- src/Navigator.Abstractions/Extensions/INavigatorExtensionOptions.cs
+++ src/Navigator.Abstractions/Extensions/INavigatorExtensionOptions.cs
@@ -2,7 +2,3 @@ namespace Navigator.Abstractions.Extensions;
 
 public interface INavigatorExtensionOptions<TExtension>
     where TExtension : INavigatorExtension;
-
-public interface INavigatorExtension<TExtension, TOptions>
-    where TOptions : INavigatorExtensionOptions<TExtension>
-    where TExtension : INavigatorExtension;
