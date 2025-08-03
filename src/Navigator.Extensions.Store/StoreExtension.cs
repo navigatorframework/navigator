d53f47f00da22ad5c6818c42a5abb98706b6140f
Lucas Maximiliano Marino <hello@lucasmarino.me>
2025-08-03 17:27:56 +0200

    refactor: separate StoreOptions into its own file and enhance configuration capabilities for database context and hybrid caching

--- src/Navigator.Extensions.Store/StoreExtension.cs
+++ src/Navigator.Extensions.Store/StoreExtension.cs
@@ -1,9 +1,7 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Navigator.Abstractions.Extensions;
 using Navigator.Abstractions.Pipelines.Steps;
 using Navigator.Configuration.Options;
-using Navigator.Extensions.Store.Persistence.Context;
 using Navigator.Extensions.Store.Steps;
 
 namespace Navigator.Extensions.Store;
@@ -15,28 +13,5 @@ public class StoreExtension : INavigatorExtension<StoreOptions>
         extensionOptions.RunDbContextConfiguration(services);
         
         services.AddScoped<INavigatorPipelineStep, RegisterConversationStep>();
-    }
-}
-
-public class StoreOptions : INavigatorExtensionOptions
-{
-    private Action<IServiceCollection>? ConfigureDbContext { get; set; }
-
-    public void ConfigureStore(Action<DbContextOptionsBuilder> options)
-    {
-        ConfigureStore<NavigatorStoreDbContext>(options);
-    }
-    
-    public void ConfigureStore<TDbContext>(Action<DbContextOptionsBuilder> options) where TDbContext : NavigatorStoreDbContext
-    {
-        ConfigureDbContext = serviceCollection =>
-        {
-            serviceCollection.AddDbContext<TDbContext>(options);
-        };
-    }
-
-    internal void RunDbContextConfiguration(IServiceCollection services)
-    {
-        ConfigureDbContext?.Invoke(services);
     }
 }
