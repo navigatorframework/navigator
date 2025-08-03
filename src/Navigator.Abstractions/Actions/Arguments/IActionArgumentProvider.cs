2b53476a203c3f02d10488922f7755b20352a0ff
Lucas Maximiliano Marino <hello@lucasmarino.me>
2025-08-03 20:03:49 +0200

    docs: add documentation for IActionArgumentProvider

--- src/Navigator.Abstractions/Actions/Arguments/IActionArgumentProvider.cs
+++ src/Navigator.Abstractions/Actions/Arguments/IActionArgumentProvider.cs
@@ -2,8 +2,24 @@ using Telegram.Bot.Types;
 
 namespace Navigator.Abstractions.Actions.Arguments;
 
+/// <summary>
+///     Interface for providing arguments for the condition and handler delegates of a <see cref="BotAction" />.
+/// </summary>
 public interface IActionArgumentProvider
 {
+    /// <summary>
+    ///     Returns the arguments for the condition delegate.
+    /// </summary>
+    /// <param name="update">The <see cref="Update" /> object.</param>
+    /// <param name="action">The <see cref="BotAction" />.</param>
+    /// <returns></returns>
     public ValueTask<object?[]> GetConditionArguments(Update update, BotAction action);
+    
+    /// <summary>
+    ///     Returns the arguments for the handler delegate.
+    /// </summary>
+    /// <param name="update">The <see cref="Update" /> object.</param>
+    /// <param name="action">The <see cref="BotAction" />.</param>
+    /// <returns></returns>
     public ValueTask<object?[]> GetHandlerArguments(Update update, BotAction action);
 }
