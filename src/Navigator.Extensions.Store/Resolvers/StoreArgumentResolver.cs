ba27cae244df0e89bf68a8d576568bb088587d83
Lucas Maximiliano Marino <hello@lucasmarino.me>
2025-08-03 17:27:41 +0200

    feat: implement StoreArgumentResolver for dynamic argument resolution of User, Chat, and Conversation types

---
+++ src/Navigator.Extensions.Store/Resolvers/StoreArgumentResolver.cs
@@ -0,0 +1,45 @@
+using Navigator.Abstractions.Actions;
+using Navigator.Abstractions.Actions.Arguments;
+using Navigator.Abstractions.Telegram;
+using Navigator.Extensions.Store.Entities;
+using Navigator.Extensions.Store.Services;
+using Telegram.Bot.Types;
+using Chat = Navigator.Extensions.Store.Entities.Chat;
+using User = Navigator.Extensions.Store.Entities.User;
+
+namespace Navigator.Extensions.Store.Resolvers;
+
+public class StoreArgumentResolver : IArgumentResolver
+{
+    private readonly INavigatorStore _store;
+
+    public StoreArgumentResolver(INavigatorStore store)
+    {
+        _store = store;
+    }
+
+    public async ValueTask<object?> GetArgument(Type type, Update update, BotAction action)
+    {
+        return type switch
+        {
+            not null when type == typeof(User) && update.GetUserOrDefault() is { } telegramUser
+                => await _store.GetUserAsync(telegramUser.Id),
+            not null when type == typeof(Chat) && update.GetChatOrDefault() is { } telegramChat
+                => await _store.GetChatAsync(telegramChat.Id),
+            not null when type == typeof(Conversation)
+                => GetConversation(update),
+            _ => null
+        };
+
+        async Task<Conversation?> GetConversation(Update telegramUpdate)
+        {
+            var telegramUser = telegramUpdate.GetUserOrDefault();
+            
+            if (telegramUser == null) return null;
+            
+            var telegramChat = telegramUpdate.GetChatOrDefault();
+            
+            return await _store.GetConversationAsync(telegramUser.Id, telegramChat?.Id);
+        }
+    }
+}
