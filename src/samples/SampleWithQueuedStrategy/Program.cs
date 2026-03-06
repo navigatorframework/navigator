cedda995244b5bc797ed6075b1617e0a05a55c01
Lucas Maximiliano Marino <hello@lucasmarino.me>
2026-03-06 15:35:32 +0100

    feat: add sample project demonstrating QueuedStrategy usage and configuration

---
+++ src/samples/SampleWithQueuedStrategy/Program.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Threading.Tasks;
+using Incremental.Common.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Navigator;
+using Navigator.Abstractions.Client;
+using Navigator.Catalog.Factory.Extensions;
+using Navigator.Configuration;
+using Navigator.Configuration.Options;
+using Navigator.Strategies.Queued;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddCommonConfiguration();
+
+builder.Services.AddMemoryCache();
+
+builder.Services.AddNavigator(configuration =>
+{
+    configuration.Options.SetWebHookBaseUrl(builder.Configuration["BASE_WEBHOOK_URL"]!);
+    configuration.Options.SetTelegramToken(builder.Configuration["TELEGRAM_TOKEN"]!);
+    configuration.Options.EnableMultipleActionsUsage();
+
+    configuration.WithStrategy<QueuedStrategy, QueuedStrategyOptions>(options =>
+    {
+        options.MaxMessagesPerQueue = 100;
+    });
+});
+
+var app = builder.Build();
+
+var bot = app.GetBot();
+
+bot.OnCommand("join", async (INavigatorClient client, Chat chat, string[] parameters) =>
+{
+    var result = string.Join(',', parameters);
+
+    await client.SendMessage(chat, result);
+});
+
+bot.OnMessage((Update _) => true, async (INavigatorClient client, Chat chat, Message message) =>
+{
+    var text = $"message received: {message.MessageId}";
+
+    await client.SendMessage(chat, text);
+});
+
+bot.OnCommand("test", async (INavigatorClient client, Chat chat, Update update) =>
+{
+    var receivedAt = update.Message?.Date;
+    var processedAt = DateTime.UtcNow;
+    await Task.Delay(5000);
+    var currentTime = DateTime.UtcNow;
+    await client.SendMessage(chat, $"message received: {receivedAt}, processed: {processedAt}, now: {currentTime}");
+});
+
+app.MapNavigator();
+
+app.Run();
