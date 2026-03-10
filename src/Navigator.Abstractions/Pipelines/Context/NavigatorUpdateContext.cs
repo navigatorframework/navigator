1415ba0228408d40e450b0747b70d9a1c65c7d2d
Lucas Maximiliano Marino <hello@lucasmarino.me>
2026-03-10 15:12:34 +0100

    feat: extend NavigatorUpdateContext with timestamp properties (ReceivedAt, SentAt)

--- src/Navigator.Abstractions/Pipelines/Context/NavigatorUpdateContext.cs
+++ src/Navigator.Abstractions/Pipelines/Context/NavigatorUpdateContext.cs
@@ -1,3 +1,4 @@
+using Navigator.Abstractions.Telegram;
 using Telegram.Bot.Types;
 
 namespace Navigator.Abstractions.Pipelines.Context;
@@ -8,9 +9,19 @@ namespace Navigator.Abstractions.Pipelines.Context;
 public class NavigatorUpdateContext
 {
     /// <summary>
-    ///     The Telegram update.
+    ///     The original Telegram update that triggered this context.
     /// </summary>
     public readonly Update Update;
+
+    /// <summary>
+    ///     The timestamp when the update was received by the bot.
+    /// </summary>
+    public readonly DateTime ReceivedAt;
+
+    /// <summary>
+    ///     The timestamp when the update was originally sent, if available.
+    /// </summary>
+    public readonly DateTime? SentAt;
 
     /// <summary>
     ///     Initializes a new instance of the <see cref="NavigatorUpdateContext"/> class.
@@ -19,5 +30,7 @@ public class NavigatorUpdateContext
     public NavigatorUpdateContext(Update update)
     {
         Update = update;
+        ReceivedAt = DateTime.UtcNow;
+        SentAt = update.GetDateOrDefault();
     }
 }
