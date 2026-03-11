0a6ba95df94acb1d04f5f831e42b5b6ddf19d5d9
Lucas Maximiliano Marino <hello@lucasmarino.me>
2026-03-11 14:35:52 +0100

    feat: add NavigatorTracerFactory and NavigatorTracerContext for enhanced tracing support

---
+++ src/Navigator/Introspection/NavigatorTracerContext.cs
@@ -0,0 +1,38 @@
+using Microsoft.Extensions.Logging;
+
+namespace Navigator.Introspection;
+
+public class NavigatorTracerContext
+{
+    private readonly ILogger<NavigatorTracerContext> _logger;
+    private readonly Stack<string> _identifiers = new();
+
+    public NavigatorTracerContext(ILogger<NavigatorTracerContext> logger)
+    {
+        _logger = logger;
+    }
+
+    public string? PeekOrDefault() => _identifiers.TryPeek(out var identifier) ? identifier : null;
+
+    public void Push(string identifier) => _identifiers.Push(identifier);
+
+    public void Pop(string expectedIdentifier)
+    {
+        if (!_identifiers.TryPeek(out var identifier))
+        {
+            _logger.LogError("Cannot pop trace {TraceIdentifier} because the tracer context stack is empty", expectedIdentifier);
+            return;
+        }
+
+        if (identifier != expectedIdentifier)
+        {
+            _logger.LogError(
+                "Cannot pop trace {TraceIdentifier} because the current tracer context identifier is {CurrentTraceIdentifier}",
+                expectedIdentifier,
+                identifier);
+            return;
+        }
+
+        _identifiers.Pop();
+    }
+}
---
+++ src/Navigator/Introspection/NavigatorTracerFactory.cs
@@ -0,0 +1,37 @@
+using Navigator.Abstractions.Introspection;
+using Navigator.Abstractions.Introspection.Sink;
+
+namespace Navigator.Introspection;
+
+public class NavigatorTracerFactory<TCategoryName> : INavigatorTracerFactory<TCategoryName>
+{
+    private readonly INavigatorTracerSink _sink;
+    private readonly NavigatorTracerContext _context;
+
+    public NavigatorTracerFactory(INavigatorTracerSink sink, NavigatorTracerContext context)
+    {
+        _sink = sink;
+        _context = context;
+    }
+
+    public INavigatorTracer Get(string? identifier = null)
+    {
+        var lastKnownIdentifier = _context.PeekOrDefault();
+        
+        async ValueTask CommitTrace(NavigatorTrace trace)
+        {
+            _context.Pop(trace.Identifier);
+            await _sink.Store(trace);
+        }
+        
+        var tracer = new NavigatorTracer(CommitTrace, identifier, lastKnownIdentifier, GetCategoryName());
+        
+        _context.Push(tracer.Identifier);
+
+        return tracer;
+
+    }
+    
+    private static string GetCategoryName() => typeof(TCategoryName).FullName ?? "_Unknown";
+    
+}
