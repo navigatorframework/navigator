3dfc8629b5baf5bef0940ff1cdcc070f97c9e57c
Lucas Maximiliano Marino <hello@lucasmarino.me>
2026-03-11 20:26:38 +0100

    feat: enhance NavigatorTrace with tag support, status tracking, and additional properties

--- src/Navigator.Abstractions/Introspection/NavigatorTrace.cs
+++ src/Navigator.Abstractions/Introspection/NavigatorTrace.cs
@@ -2,18 +2,31 @@ namespace Navigator.Abstractions.Introspection;
 
 public class NavigatorTrace
 {
-    public readonly string Identifier;
-    public readonly string? ParentIdentifier;
-    public readonly DateTimeOffset StartTime;
-    public required string SourceContext { get; init; }
     
-    public DateTimeOffset? EndTime { get; set; }
-    public NavigatorTrace(string? identifier, string? parentIdentifier)
+    public string Identifier { get; init; }
+    public string? ParentIdentifier { get; init; }
+    public string SourceContext { get; init; }
+    public DateTimeOffset StartTime { get; init; }
+    public Dictionary<string, HashSet<string>> Tags { get; init; }
+    
+    public NavigatorTrace(string? identifier, string? parentIdentifier, string sourceContext)
     {
         Identifier = identifier ?? $"{Guid.CreateVersion7()}";
         ParentIdentifier = parentIdentifier;
+        SourceContext = sourceContext;
         StartTime = TimeProvider.System.GetUtcNow();
+        Tags = new Dictionary<string, HashSet<string>>();
     }
     
-    
+    public DateTimeOffset? EndTime { get; set; }
+    public ENavigatorTraceStatus Status { get; set; }
+    public string? StatusType { get; set; }
+    public string? StatusMessage { get; set; }
+}
+
+public enum ENavigatorTraceStatus
+{
+    Ok,
+    Warning,
+    Error
 }
