196d43a6f9a91ba991a873f648f2d75d4bc1aa70
Lucas Maximiliano Marino <hello@lucasmarino.me>
2026-03-16 18:32:25 +0100

    feat: register ITraceFormatter in ManagementExtension for trace formatting

--- src/Navigator.Extensions.Management/ManagementExtension.cs
+++ src/Navigator.Extensions.Management/ManagementExtension.cs
@@ -1,16 +1,18 @@
 using Microsoft.Extensions.DependencyInjection;
 using Navigator.Abstractions.Extensions;
 using Navigator.Configuration.Options;
+using Navigator.Extensions.Management.Services;
 
 namespace Navigator.Extensions.Management;
 
 /// <summary>
 ///     Extension for adding management capabilities to Navigator.
 /// </summary>
-public class ManagementExtension : INavigatorExtension<ManagementOptions>
+public sealed class ManagementExtension : INavigatorExtension<ManagementOptions>
 {
+    /// <inheritdoc />
     public void Configure(IServiceCollection services, NavigatorOptions navigatorOptions, ManagementOptions extensionOptions)
     {
-        throw new NotImplementedException();
+        services.AddSingleton<ITraceFormatter, TraceFormatter>();
     }
 }
