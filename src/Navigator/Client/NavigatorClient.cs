df5a6038ad80f0e1fc7260467c7cabd8ac69d374
Lucas Maximiliano Marino <hello@lucasmarino.me>
2026-04-28 00:20:01 +0200

    feat: add tracing support to NavigatorClient with Telegram API call introspection

--- src/Navigator/Client/NavigatorClient.cs
+++ src/Navigator/Client/NavigatorClient.cs
@@ -1,9 +1,12 @@
 using Microsoft.Extensions.Options;
 using Navigator.Abstractions.Client;
 using Navigator.Abstractions.Entities;
+using Navigator.Abstractions.Introspection;
 using Navigator.Configuration.Options;
 using Telegram.Bot;
 using Telegram.Bot.Requests;
+using Telegram.Bot.Requests.Abstractions;
+using Telegram.Bot.Types;
 
 namespace Navigator.Client;
 
@@ -12,13 +15,17 @@ namespace Navigator.Client;
 /// </summary>
 public class NavigatorClient : TelegramBotClient, INavigatorClient
 {
+    private readonly INavigatorTracerFactory<NavigatorClient> _tracerFactory;
+    
     /// <summary>
     /// Builds a <see cref="NavigatorClient"/>.
     /// </summary>
     /// <param name="options"><see cref="NavigatorOptions"/></param>
     /// <exception cref="ArgumentNullException">If telegram token is null</exception>
-    public NavigatorClient(IOptions<NavigatorOptions> options) : base(options.Value.GetTelegramToken() ?? throw new ArgumentNullException())
+    public NavigatorClient(IOptions<NavigatorOptions> options, INavigatorTracerFactory<NavigatorClient> tracerFactory) 
+        : base(options.Value.GetTelegramToken() ?? throw new ArgumentNullException())
     {
+        _tracerFactory = tracerFactory;
     }
 
     /// <inheritdoc />
@@ -35,4 +42,31 @@ public class NavigatorClient : TelegramBotClient, INavigatorClient
             SupportsInlineQueries = bot.SupportsInlineQueries
         };
     }
+
+    #region Overrides of TelegramBotClient
+
+    /// <inheritdoc />
+    public override async Task<TResponse> SendRequest<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = new CancellationToken())
+    {
+        if (!_tracerFactory.HasActiveTrace())
+        {
+            return await base.SendRequest(request, cancellationToken);
+        }
+
+        await using var tracer = _tracerFactory.Get();
+            
+        tracer.AddTag(NavigatorTraceKeys.TelegramApiCall, $"{request.HttpMethod}: {request.MethodName}");
+
+        var response = await base.SendRequest(request, cancellationToken);
+
+        if (response is Message message)
+        {
+            tracer.AddTag(NavigatorTraceKeys.UpdateChatId, $"{message.Chat.Id}");
+            tracer.AddTag(NavigatorTraceKeys.UpdateMessageId, $"{message.Id}");
+        }
+        
+        return response;
+    }
+
+    #endregion
 }
